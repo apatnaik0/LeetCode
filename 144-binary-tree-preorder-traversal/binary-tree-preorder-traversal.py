0a651c0cd8951f2ef84337638b5f383b8499b07a
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-06-29 20:31:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.10%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -16,18 +16,38 @@ class Solution:
         # preorder(root)
         # return ans
 
-        if root is None:
-            return []
-        ans = []
-        st = []
-        st.append(root)
-        while st:
-            node = st.pop()
-            ans.append(node.val)
-            if node.right:
-                st.append(node.right)
-            if node.left:
-                st.append(node.left)
-        return ans
+        # if root is None:
+        #     return []
+        # ans = []
+        # st = []
+        # st.append(root)
+        # while st:
+        #     node = st.pop()
+        #     ans.append(node.val)
+        #     if node.right:
+        #         st.append(node.right)
+        #     if node.left:
+        #         st.append(node.left)
+        # return ans
+
+        preorder = []
+        cur = root
+        while cur:
+            if cur.left:
+                node = cur.left
+                while node.right and node.right!=cur:
+                    node = node.right
+                if not node.right:
+                    node.right = cur
+                    preorder.append(cur.val)
+                    cur = cur.left
+                if node.right == cur:
+                    node.right = None
+                    cur = cur.right
+                
+            else:
+                preorder.append(cur.val)
+                cur = cur.right
+        return preorder
             
         
