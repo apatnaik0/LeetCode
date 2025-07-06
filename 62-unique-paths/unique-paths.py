5382dce9b58aadd461ec35fbaa18f16963c69bbd
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-06 16:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.35%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -13,19 +13,22 @@ class Solution:
         # dp[0][0]=1
         # return solve(m-1,n-1,dp)
 
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        # dp = [[0 for _ in range(n)] for _ in range(m)]
+        prev = [0 for _ in range(n)]
         for i in range(0,m):
+            cur = [0 for _ in range(n)]
             for j in range(0,n):
                 if i==0 and j == 0:
-                    dp[i][j]=1
+                    cur[j]=1
                 else:
                     up = 0
                     left = 0
                     if i>0:
-                        up = dp[i-1][j]
+                        up = prev[j]
                     if j>0:
-                        left = dp[i][j-1]
-                    dp[i][j] = up + left
+                        left = cur[j-1]
+                    cur[j] = up + left
+            prev = cur
 
-        return dp[m-1][n-1]
+        return prev[n-1]
         
