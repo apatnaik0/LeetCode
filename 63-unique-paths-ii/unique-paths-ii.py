cb179426e8f548f5b1dfcc779ac45636437a975b
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-06 17:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (91.58%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,18 +1,35 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        def solve(i,j,dp):
-            if i>=0 and j>=0 and obstacleGrid[i][j]==1:
-                return 0
-            if i==0 and j==0:
-                return 1
-            if i<0 or j<0:
-                return 0
-            if dp[i][j]!=0:
-                return dp[i][j]
-            up = solve(i-1,j,dp)
-            left = solve(i,j-1,dp)
-            dp[i][j] = up + left
-            return dp[i][j]
+        # def solve(i,j,dp):
+        #     if i>=0 and j>=0 and obstacleGrid[i][j]==1:
+        #         return 0
+        #     if i==0 and j==0:
+        #         return 1
+        #     if i<0 or j<0:
+        #         return 0
+        #     if dp[i][j]!=0:
+        #         return dp[i][j]
+        #     up = solve(i-1,j,dp)
+        #     left = solve(i,j-1,dp)
+        #     dp[i][j] = up + left
+        #     return dp[i][j]
+
+        # dp = [[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
+        # return solve(len(obstacleGrid)-1,len(obstacleGrid[0])-1,dp)
 
         dp = [[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
-        return solve(len(obstacleGrid)-1,len(obstacleGrid[0])-1,dp)
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if i>=0 and j>=0 and obstacleGrid[i][j]==1:
+                    dp[i][j]=0
+                elif i==0 and j==0:
+                    dp[i][j]=1
+                else:
+                    up = 0
+                    left = 0
+                    if j>0:
+                        left = dp[i][j-1]
+                    if i>0:
+                        up = dp[i-1][j]
+                    dp[i][j] = up + left
+        return dp[len(obstacleGrid)-1][len(obstacleGrid[0])-1]
