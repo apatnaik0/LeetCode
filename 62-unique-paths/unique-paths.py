253f37e35eb5c5a9d83161c17db1231763eac916
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-06 16:15:16 +0530

    Time: 3 ms (21.99%) | Memory: 17.7 MB (88.35%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,15 +1,31 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def solve(i,j,dp):
-            if i<0 or j<0:
-                return 0
-            if dp[i][j]!=0:
-                return dp[i][j]
-            left = solve(i-1,j,dp)
-            up = solve(i,j-1,dp)
-            dp[i][j] = left + up
-            return dp[i][j]
+        # def solve(i,j,dp):
+        #     if i<0 or j<0:
+        #         return 0
+        #     if dp[i][j]!=0:
+        #         return dp[i][j]
+        #     left = solve(i-1,j,dp)
+        #     up = solve(i,j-1,dp)
+        #     dp[i][j] = left + up
+        #     return dp[i][j]
+        # dp = [[0 for _ in range(n)] for _ in range(m)]
+        # dp[0][0]=1
+        # return solve(m-1,n-1,dp)
+
         dp = [[0 for _ in range(n)] for _ in range(m)]
-        dp[0][0]=1
-        return solve(m-1,n-1,dp)
+        for i in range(0,m):
+            for j in range(0,n):
+                if i==0 and j == 0:
+                    dp[i][j]=1
+                else:
+                    up = 0
+                    left = 0
+                    if i>0:
+                        up = dp[i-1][j]
+                    if j>0:
+                        left = dp[i][j-1]
+                    dp[i][j] = up + left
+
+        return dp[m-1][n-1]
         
