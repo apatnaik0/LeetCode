a638ec32d708aa2290622262532c044bcdc12ca8
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-06 21:48:32 +0530

    Time: 19 ms (38.19%) | Memory: 20.1 MB (51.96%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -2,18 +2,36 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)-1
         n = len(grid[0])-1
-        def solve(i,j,dp):
-            if i == 0 and j == 0:
-                return grid[i][j]
-            if i<0 or j<0:
-                return int(1e9)
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            left = grid[i][j] + solve(i,j-1,dp)
-            up = grid[i][j] + solve(i-1,j,dp)
-            dp[i][j] = min(left,up)
-            return dp[i][j]
+        # def solve(i,j,dp):
+        #     if i == 0 and j == 0:
+        #         return grid[i][j]
+        #     if i<0 or j<0:
+        #         return int(1e9)
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     left = grid[i][j] + solve(i,j-1,dp)
+        #     up = grid[i][j] + solve(i-1,j,dp)
+        #     dp[i][j] = min(left,up)
+        #     return dp[i][j]
         dp = [[-1 for _ in range(n+1)] for _ in range(m+1)]
-        return solve(m,n,dp)
+        # return solve(m,n,dp)
+
+        for i in range(m+1):
+            for j in range(n+1):
+                if i==0 and j==0:
+                    dp[i][j]=grid[i][j]
+                else:
+                    left = grid[i][j]
+                    if j>0:
+                        left += dp[i][j-1]
+                    else:
+                        left += int(1e9)
+                    up = grid[i][j]
+                    if i>0:
+                        up += dp[i-1][j]
+                    else:
+                        up += int(1e9)
+                    dp[i][j] = min(left,up)
+        return dp[m][n]
         
         
