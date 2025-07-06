07de512e488269f5d0d22f5606e1cd2cafea61b2
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-07 00:20:14 +0530

    Time: 7 ms (36.07%) | Memory: 18.8 MB (82.84%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -10,14 +10,16 @@ class Solution:
         #     diag = triangle[i][j] + solve(i+1,j+1)
         #     dp[i][j] = min(down,diag)
         #     return dp[i][j]
-        dp = [[-1 for _ in range(n)] for _ in range(n)]
+        prev = [-1 for _ in range(n)]
         
         for j in range(n):
-            dp[n-1][j] = triangle[n-1][j]
+            prev[j] = triangle[n-1][j]
         
         for i in range(n-2,-1,-1):
+            cur= [-1 for _ in range(n)]
             for j in range(i,-1,-1):
-                down = triangle[i][j] + dp[i+1][j]
-                diag = triangle[i][j] + dp[i+1][j+1]
-                dp[i][j] = min(down,diag)
-        return dp[0][0]
+                down = triangle[i][j] + prev[j]
+                diag = triangle[i][j] + prev[j+1]
+                cur[j] = min(down,diag)
+            prev = cur
+        return prev[0]
