6d4bc33a6d3d71ddd1fba3fa5cb504e35d68e186
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-08 00:39:02 +0530

    Time: 1015 ms (25.50%) | Memory: 19.8 MB (28.91%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,25 +1,37 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        def solve(ind,target):
-            if target == 0:
-                return 0
-            if ind == 0:
-                if target%coins[ind]==0:
-                    return target//coins[ind]
-                return 1e9
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            pick = 1e9
-            if target>=coins[ind]:
-                pick = 1 + solve(ind,target-coins[ind])
-            notpick = solve(ind-1,target)
-            dp[ind][target] = min(pick,notpick)
-            return dp[ind][target]
+        # def solve(ind,target):
+        #     if ind == 0:
+        #         if target%coins[ind]==0:
+        #             return target//coins[ind]
+        #         return 1e9
+        #     if dp[ind][target]!=-1:
+        #         return dp[ind][target]
+        #     pick = 1e9
+        #     if target>=coins[ind]:
+        #         pick = 1 + solve(ind,target-coins[ind])
+        #     notpick = solve(ind-1,target)
+        #     dp[ind][target] = min(pick,notpick)
+        #     return dp[ind][target]
         
         dp = [[-1 for _ in range(amount+1)] for _ in range(n)]
-        ans = solve(n-1,amount)
-        if ans>=1e9:
+        # ans = solve(n-1,amount)
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i] = i//coins[0]
+            else:
+                dp[0][i] = 1e9
+        for i in range(n):
+            dp[i][0] = 0
+        for i in range(1,n):
+            for j in range(1,amount+1):
+                pick = 1e9
+                if j>=coins[i]:
+                    pick = 1 + dp[i][j-coins[i]]
+                notpick = dp[i-1][j]
+                dp[i][j] = min(pick,notpick)
+        if dp[n-1][amount]>=1e9:
             return -1
-        return ans
+        return dp[n-1][amount]
             
