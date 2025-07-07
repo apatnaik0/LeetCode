37259749771be53cb06e8a1a9848d2878e9a448f
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-07 18:25:51 +0530

    Time: 1950 ms (19.57%) | Memory: 33.9 MB (34.77%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,27 +1,40 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        def solve(ind,t):
-            if t==0:
-                return True
-            if ind==0:
-                return nums[0]==t
+        # def solve(ind,t):
+        #     if t==0:
+        #         return True
+        #     if ind==0:
+        #         return nums[0]==t
             
-            if dp[ind][t]!=-1:
-                return dp[ind][t]
+        #     if dp[ind][t]!=-1:
+        #         return dp[ind][t]
             
-            notpick = solve(ind-1,t)
-            pick = False
-            if nums[ind]<=t:
-                pick = solve(ind-1,t-nums[ind])
-            dp[ind][t] = pick or notpick
-            return dp[ind][t]
+        #     notpick = solve(ind-1,t)
+        #     pick = False
+        #     if nums[ind]<=t:
+        #         pick = solve(ind-1,t-nums[ind])
+        #     dp[ind][t] = pick or notpick
+        #     return dp[ind][t]
         
         s = 0
         for i in nums:
             s += i
         if s%2:
             return False
-        dp = [[-1 for _ in range(s//2+1)] for _ in range(len(nums))]
-        return solve(len(nums)-1,s//2)
+        dp = [[False for _ in range(s//2+1)] for _ in range(len(nums))]
+        # return solve(len(nums)-1,s//2)
+        for i in range(len(nums)):
+            dp[i][0] = True
+        if nums[0]<=s//2+1:
+            dp[0][nums[0]] = True
+    
+        for i in range(1,len(nums)):
+            for j in range(1,s//2+1):
+                pick = False
+                if nums[i]<=j:
+                    pick = dp[i-1][j-nums[i]]
+                notpick = dp[i-1][j]
+                dp[i][j] = pick or notpick
+        return dp[len(nums)-1][s//2]
 
             
