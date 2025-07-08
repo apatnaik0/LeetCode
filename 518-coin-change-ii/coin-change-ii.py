db66031b3a24d08325e6c06058c613cfd2b11cf0
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-08 16:26:40 +0530

    Time: 371 ms (38.05%) | Memory: 63 MB (23.93%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,20 +1,32 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n= len(coins)
-        def solve(ind,target):
-            if ind == 0:
-                if target == 0:
-                    return 1
-                if target%coins[0]==0:
-                    return 1
-                return 0
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            notpick = solve(ind-1,target)
-            pick = 0
-            if target>=coins[ind]:
-                pick = solve(ind,target-coins[ind])
-            dp[ind][target] = pick + notpick
-            return dp[ind][target]
-        dp = [[-1 for i in range(amount+ 1)] for j in range(n)]
-        return solve(n-1,amount)
+        # def solve(ind,target):
+        #     if ind == 0:
+        #         if target == 0:
+        #             return 1
+        #         if target%coins[0]==0:
+        #             return 1
+        #         return 0
+        #     if dp[ind][target]!=-1:
+        #         return dp[ind][target]
+        #     notpick = solve(ind-1,target)
+        #     pick = 0
+        #     if target>=coins[ind]:
+        #         pick = solve(ind,target-coins[ind])
+        #     dp[ind][target] = pick + notpick
+        #     return dp[ind][target]
+        dp = [[0 for i in range(amount+ 1)] for j in range(n)]
+        # return solve(n-1,amount)
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i]=1
+        for i in range(1,n):
+            for j in range(amount+1):
+                notpick = dp[i-1][j]
+                pick = 0
+                if j>=coins[i]:
+                    pick = dp[i][j-coins[i]]
+                dp[i][j] = pick + notpick
+        return dp[n-1][amount]
+            
