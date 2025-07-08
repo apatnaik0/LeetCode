c8006936d5a0e765adcf38a24a11304b4471de2b
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-08 15:46:27 +0530

    Time: 51 ms (80.76%) | Memory: 18 MB (80.17%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        def solve(ind,tar):
-            if ind == 0:
-                if tar==0 and nums[ind]==0:
-                    return 2
-                if tar==0 or tar==nums[ind]:
-                    return 1
-                return 0
-            if dp[ind][tar]!=-1:
-                return dp[ind][tar]
-            notpick = solve(ind-1,tar)
-            pick = 0
-            if tar>=nums[ind]:
-                pick = solve(ind-1,tar-nums[ind])
-            dp[ind][tar] = pick + notpick
-            return dp[ind][tar]
+        # def solve(ind,tar):
+        #     if ind == 0:
+        #         if tar==0 and nums[ind]==0:
+        #             return 2
+        #         if tar==0 or tar==nums[ind]:
+        #             return 1
+        #         return 0
+        #     if dp[ind][tar]!=-1:
+        #         return dp[ind][tar]
+        #     notpick = solve(ind-1,tar)
+        #     pick = 0
+        #     if tar>=nums[ind]:
+        #         pick = solve(ind-1,tar-nums[ind])
+        #     dp[ind][tar] = pick + notpick
+        #     return dp[ind][tar]
         n = len(nums)
         s = 0
         for i in nums:
@@ -25,7 +25,20 @@ class Solution:
         if tar%2!=0:
             return 0
         tar = tar//2
-        dp = [[-1 for _ in range(tar+1)] for _ in range(n)]
-        return solve(n-1,tar)
+        dp = [[0 for _ in range(tar+1)] for _ in range(n)]
+        for i in range(tar+1):
+            if i==0 and nums[0]==0:
+                dp[0][i] = 2
+            elif i == 0 or i == nums[0]:
+                dp[0][i] = 1
+
+        for i in range(1,n):
+            for j in range(tar+1):
+                notpick = dp[i-1][j]
+                pick = 0
+                if j>=nums[i]:
+                    pick = dp[i-1][j-nums[i]]
+                dp[i][j] = pick + notpick
+        return dp[n-1][tar]
 
         
