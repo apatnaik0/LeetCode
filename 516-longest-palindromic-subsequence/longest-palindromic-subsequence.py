67e245528f5930dd1240a85d6613e4dc8de12efc
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 00:32:24 +0530

    Time: 1412 ms (31.60%) | Memory: 18.2 MB (89.33%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -13,15 +13,18 @@ class Solution:
             return dp[i1][i2]
 
         rs = s[::-1]
-        dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+        prev = [0 for _ in range(n+1)]
         # return solve(n-1,n-1)
-        for i in range(n+1):
-            dp[i][0] = 0
-            dp[0][i] = 0
+        # for i in range(n+1):
+        #     dp[i][0] = 0
+        #     dp[0][i] = 0
+        # prev[0] = 0
         for i1 in range(1,n+1):
+            cur = [0 for _ in range(n+1)]
             for i2 in range(1,n+1):
                 if s[i1-1]==rs[i2-1]:
-                    dp[i1][i2] = 1 + dp[i1-1][i2-1]
+                    cur[i2] = 1 + prev[i2-1]
                 else:
-                    dp[i1][i2] = max(dp[i1][i2-1],dp[i1-1][i2])
-        return dp[n][n]
+                    cur[i2] = max(cur[i2-1],prev[i2])
+            prev = cur[:]
+        return prev[n]
