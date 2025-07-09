ee6fe45f11e4fb8923fcc505e5080fb525bfb5e4
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 23:20:06 +0530

    Time: 21 ms (5.30%) | Memory: 21.8 MB (5.22%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        p=0
-        bp=prices[0]
-        for i in prices:
-            if i>bp:
-                p+= i-bp
-            bp = i
-        return p
+        # p=0
+        # bp=prices[0]
+        # for i in prices:
+        #     if i>bp:
+        #         p+= i-bp
+        #     bp = i
+        # return p
+        def solve(i,buy):
+            if i == n:
+                return 0
+            if dp[i][buy]!=0:
+                return dp[i][buy]
+            if buy:
+                dp[i][buy] = max(-prices[i]+solve(i+1,0),solve(i+1,1))
+            else:
+                dp[i][buy] = max(prices[i]+solve(i+1,1),solve(i+1,0))
+            return dp[i][buy]
+        n = len(prices)
+        dp = [[0 for _ in range(2)] for _ in range(n+1)]
+        # return solve(0,1)
+        for i in range(n-1,-1,-1):
+            dp[i][1] = max(-prices[i]+dp[i+1][0],dp[i+1][1])
+            dp[i][0] = max(prices[i]+dp[i+1][1],dp[i+1][0])
+        return dp[0][1]
+
         
