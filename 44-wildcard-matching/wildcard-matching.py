055521ed74f4480603f4bc08e13c8a7e3229cc9e
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 20:57:02 +0530

    Time: 427 ms (31.14%) | Memory: 26.3 MB (42.32%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n1 = len(s)
+        n2 = len(p)
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        
+        # def solve(i,j):
+        #     if j==0 and i==0:
+        #         return True
+        #     elif j==0 and i>0:
+        #         return False
+        #     elif j>0 and i==0:
+        #         while j>0:
+        #             if p[j-1]!='*':
+        #                 return False
+        #             j-=1
+        #         return True 
+        #     if dp[i][j]!=0:
+        #         return dp[i][j]
+        #     if s[i-1]==p[j-1] or p[j-1]=='?':
+        #         dp[i][j] = solve(i-1,j-1)
+        #     elif p[j-1]=='*':
+        #         dp[i][j] = solve(i-1,j) or solve(i,j-1)
+        #     else:
+        #         return False
+        #     return dp[i][j]
+
+        # return solve(n1,n2)
+        dp[0][0] = True
+        for i in range(1,n1+1):
+            dp[i][0] = False
+        for j in range(1, n2 + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 1]
+            else:
+                dp[0][j] = False
+            
+
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if s[i-1]==p[j-1] or p[j-1]=='?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1]=='*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
+                else:
+                    dp[i][j] = False
+        return dp[n1][n2]
