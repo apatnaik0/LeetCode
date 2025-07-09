ffd054273e0c8296311750586e4613a8fc3b19a3
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 14:58:33 +0530

    Time: 615 ms (54.75%) | Memory: 18 MB (85.52%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -12,12 +12,15 @@ class Solution:
             else:
                 dp[i][j] = max(solve(i,j-1),solve(i-1,j))
             return dp[i][j]
-        dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+        # dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+        prev = [0 for _ in range(n+1)]
         # pl = solve(n-1,n-1)
         for i in range(1,n+1):
+            cur = [0 for _ in range(n+1)]
             for j in range(1,n+1):
                 if s[i-1]==rs[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    cur[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-        return n-dp[n][n]
+                    cur[j] = max(prev[j],cur[j-1])
+            prev = cur
+        return n-prev[n]
