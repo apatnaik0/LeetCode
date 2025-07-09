77cf6e9dcca22fc819188b3043ce9bec370bf3bc
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 15:36:45 +0530

    Time: 134 ms (40.61%) | Memory: 20.2 MB (40.48%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -13,6 +13,13 @@ class Solution:
             return dp[i][j]
         n1 = len(word1)
         n2 = len(word2)
-        dp = [[0 for _ in range(n2)] for _ in range(n1)]
-        lcs = solve(n1-1,n2-1)
-        return n1 + n2 - 2*lcs
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        # lcs = solve(n1-1,n2-1)
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+
+        return n1 + n2 - 2*dp[n1][n2]
