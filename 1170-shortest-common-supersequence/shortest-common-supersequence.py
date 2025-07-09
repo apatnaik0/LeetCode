3adbc4cc47c585963349e2643a55006704985fd7
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 16:28:14 +0530

    Time: 452 ms (44.49%) | Memory: 43.6 MB (43.17%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,19 +1,25 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        def solve(i,j):
-            if i==0 or j==0:
-                return 0
-            if dp[i][j]!=0:
-                return dp[i][j]
-            if str1[i-1]==str2[j-1]:
-                dp[i][j] = 1 + solve(i-1,j-1)
-            else:
-                dp[i][j] = max(solve(i,j-1),solve(i-1,j))
-            return dp[i][j]
+        # def solve(i,j):
+        #     if i==0 or j==0:
+        #         return 0
+        #     if dp[i][j]!=0:
+        #         return dp[i][j]
+        #     if str1[i-1]==str2[j-1]:
+        #         dp[i][j] = 1 + solve(i-1,j-1)
+        #     else:
+        #         dp[i][j] = max(solve(i,j-1),solve(i-1,j))
+        #     return dp[i][j]
         n1 = len(str1)
         n2 = len(str2)
         dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
-        solve(n1,n2)
+        # solve(n1,n2)
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if str1[i-1]==str2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
         i = n1
         j = n2
         ans = ""
