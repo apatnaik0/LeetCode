7f865be80451b0f67ca788969e1e9252d09ba368
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-09 18:38:30 +0530

    Time: 55 ms (75.96%) | Memory: 18 MB (92.12%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -3,18 +3,24 @@ class Solution:
         
         n1 = len(word1)
         n2 = len(word2)
-        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        # dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        prev = [0 for _ in range(n2+1)]
 
-        for i in range(n1+1):
-            dp[i][0] = i
+        # for i in range(n1+1):
+        #     dp[i][0] = i
         for i in range(n2+1):
-            dp[0][i] = i
+            prev[i] = i
+
+
 
         for i in range(1,n1+1):
+            cur = [0 for _ in range(n2+1)]
+            cur[0] = i
             for j in range(1,n2+1):
                 if word1[i-1]==word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
+                    cur[j] = prev[j-1]
                 else:
-                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j])
-        return dp[n1][n2]
+                    cur[j] = 1 + min(prev[j-1], cur[j-1], prev[j])
+            prev = cur
+        return prev[n2]
     
