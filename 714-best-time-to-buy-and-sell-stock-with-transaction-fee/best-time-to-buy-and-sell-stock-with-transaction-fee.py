71d5d242f13233570ccead34c3b4cc523129d6b3
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-10 16:09:13 +0530

    Time: 139 ms (50.45%) | Memory: 38.9 MB (22.89%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -11,9 +11,12 @@ class Solution:
             else:
                 dp[i][buy] = max(prices[i] - fee + solve(i+1,1),solve(i+1,0))
             return dp[i][buy]
-        dp = [[0 for _ in range(2)] for _ in range(n+1)]
+        # dp = [[0 for _ in range(2)] for _ in range(n+1)]
+        prev = [0 for _ in range(2)]
+        cur = [0 for _ in range(2)]
         # return solve(0,1)
         for i in range(n-1,-1,-1):
-            dp[i][1] = max(-prices[i] + dp[i+1][0],dp[i+1][1])
-            dp[i][0] = max(prices[i] - fee + dp[i+1][1],dp[i+1][0])
-        return dp[0][1]
+            cur[1] = max(-prices[i] + prev[0],prev[1])
+            cur[0] = max(prices[i] - fee + prev[1],prev[0])
+            prev = cur
+        return prev[1]
