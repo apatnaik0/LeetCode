9b84be9d8ad5fd56d023063bd1338fb873139cc1
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-10 16:07:42 +0530

    Time: 344 ms (25.52%) | Memory: 42.3 MB (21.28%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -12,4 +12,8 @@ class Solution:
                 dp[i][buy] = max(prices[i] - fee + solve(i+1,1),solve(i+1,0))
             return dp[i][buy]
         dp = [[0 for _ in range(2)] for _ in range(n+1)]
-        return solve(0,1)
+        # return solve(0,1)
+        for i in range(n-1,-1,-1):
+            dp[i][1] = max(-prices[i] + dp[i+1][0],dp[i+1][1])
+            dp[i][0] = max(prices[i] - fee + dp[i+1][1],dp[i+1][0])
+        return dp[0][1]
