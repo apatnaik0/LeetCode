3c4e1a5ed863df7826d0c0c5ee8eb75a2c709168
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-10 22:35:14 +0530

    Time: 5005 ms (5.00%) | Memory: 258.1 MB (5.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -11,13 +11,16 @@ class Solution:
                 ans = max(ans, 1 + solve(i+1,i))
             return ans
         dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+        prev = [0 for _ in range(n+1)]
         # return solve(0,-1)
         for i in range(n-1,-1,-1):
+            cur = [0 for _ in range(n+1)]
             for pi in range(i-1,-2,-1):
-                dp[i][pi+1] = dp[i+1][pi+1]
+                cur[pi+1] = prev[pi+1]
                 if pi==-1 or nums[i]>nums[pi]:
-                    dp[i][pi+1] = max(dp[i][pi+1], 1 + dp[i+1][i+1])
-        return dp[0][0]
+                    cur[pi+1] = max(cur[pi+1], 1 + prev[i+1])
+            prev = cur
+        return prev[0]
         
                 
 
