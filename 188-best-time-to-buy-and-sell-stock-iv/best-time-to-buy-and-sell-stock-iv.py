f776cb5aeecced5a84b99d1ffbaa5b876f90051f
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-10 14:39:17 +0530

    Time: 70 ms (49.24%) | Memory: 18 MB (71.43%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -12,12 +12,15 @@ class Solution:
             return dp[i][t]
 
         n = len(prices)
-        dp = [[0 for _ in range(2*k+1)] for _ in range(n+1)]
+        # dp = [[0 for _ in range(2*k+1)] for _ in range(n+1)]
+        prev = [0 for _ in range(2*k+1)]
         # return solve(0,2*k)
         for i in range(n-1,-1,-1):
+            cur = [0 for _ in range(2*k+1)]
             for t in range(1,2*k+1):
                 if t%2==0:
-                    dp[i][t] = max(-prices[i]+dp[i+1][t-1],dp[i+1][t])
+                    cur[t] = max(-prices[i]+prev[t-1],prev[t])
                 else:
-                    dp[i][t] = max(prices[i]+dp[i+1][t-1],dp[i+1][t])
-        return dp[0][2*k]
+                    cur[t] = max(prices[i]+prev[t-1],prev[t])
+            prev = cur
+        return prev[2*k]
