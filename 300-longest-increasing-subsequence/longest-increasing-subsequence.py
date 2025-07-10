c2716be1d11ee4ced7ac0d85b5204cd2f1235865
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-10 23:52:55 +0530

    Time: 13 ms (73.19%) | Memory: 19.1 MB (8.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -21,23 +21,53 @@ class Solution:
         #             cur[pi+1] = max(cur[pi+1], 1 + prev[i+1])
         #     prev = cur
         # return prev[0]
-        dp = [1]*n
-        ha = [i for i in range(n)]
-        maxi = int(-1e9)
-        for cur in range(n):
-            for prev in range(cur):
-                if nums[cur] > nums[prev] and dp[prev] + 1 > dp[cur]:
-                    dp[cur] = dp[prev] + 1
-                    ha[cur] = prev
-            if dp[cur]>maxi:
-                maxi = dp[cur]
-                lastindex = cur
-        ans = [nums[lastindex]]
-        while(lastindex != ha[lastindex]):
-            lastindex = ha[lastindex]
-            ans.append(nums[lastindex])
+
+        # Return LIS
+
+        # dp = [1]*n
+        # ha = [i for i in range(n)]
+        # maxi = int(-1e9)
+        # for cur in range(n):
+        #     for prev in range(cur):
+        #         if nums[cur] > nums[prev] and dp[prev] + 1 > dp[cur]:
+        #             dp[cur] = dp[prev] + 1
+        #             ha[cur] = prev
+        #     if dp[cur]>maxi:
+        #         maxi = dp[cur]
+        #         lastindex = cur
+        # ans = [nums[lastindex]]
+        # while(lastindex != ha[lastindex]):
+        #     lastindex = ha[lastindex]
+        #     ans.append(nums[lastindex])
         
-        return len(ans[::-1])
+        # return len(ans[::-1])
+
+        # Binary Search for Length of LIS
+
+        def lower_bound(arr,num):
+            r = len(arr)-1
+            l = 0
+            ans = 0
+            while l<=r:
+                mid = (l+r)//2
+                if arr[mid]==num:
+                    return mid
+                elif arr[mid]>num:
+                    ans = mid
+                    r = mid-1
+                else:
+                    l = mid+1
+            return ans
+        
+        res = [nums[0]]
+        for i in range(1,n):
+            if nums[i]>res[-1]:
+                res.append(nums[i])
+            else:
+                ind = lower_bound(res,nums[i])
+                res[ind] = nums[i]
+            # print(res)
+        return len(res)
 
         
                 
