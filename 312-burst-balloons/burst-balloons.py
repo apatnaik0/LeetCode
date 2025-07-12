623f738263538d4c7d7d749b2c57df2c7f20cd5d
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-12 21:11:34 +0530

    Time: 2753 ms (77.51%) | Memory: 22.6 MB (55.98%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -13,5 +13,13 @@ class Solution:
             return dp[i][j]
         n = len(nums)
         nums = [1] + nums + [1]
-        dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
-        return solve(1,n)
+        dp = [[0 for _ in range(n+2)] for _ in range(n+2)]
+        # return solve(1,n)
+        for i in range(n,0,-1):
+            for j in range(i,n+1):
+                maxi = float('-inf')
+                for k in range(i,j+1):
+                    ans = nums[i-1]* nums[k] * nums[j+1] + dp[i][k-1] + dp[k+1][j]
+                    maxi = max(maxi,ans)
+                dp[i][j] = maxi
+        return dp[1][n]
