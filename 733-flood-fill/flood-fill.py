4c3b5e772d565e037f36c74f65ad97e53394c08e
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-16 16:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (70.10%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -3,15 +3,16 @@ class Solution:
     def bfs(self,sr,sc,color,old,newimage,n,m,vis):
         q = deque()
         q.append([sr,sc])
+        vis[sr][sc]=1
         delc = [0,1,0,-1]
         delr = [-1,0,1,0]
         while q:
             node = q.popleft()
             newimage[node[0]][node[1]] = color
-            vis[node[0]][node[1]]=1
             for i in range(4):
                 if node[0]+delr[i]>=0 and node[0]+delr[i]<n and node[1]+delc[i]>=0 and node[1]+delc[i]<m and newimage[node[0]+delr[i]][node[1]+delc[i]]==old and vis[node[0]+delr[i]][node[1]+delc[i]]!=1:
                     q.append([node[0]+delr[i],node[1]+delc[i]])
+                    vis[node[0]+delr[i]][node[1]+delc[i]]=1
         return newimage
 
 
