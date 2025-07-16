9c713b773c381e6cbb4870aebbc24b3ffd45e6e9
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-16 16:28:17 +0530

    Time: 2 ms (19.70%) | Memory: 18.2 MB (27.91%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def bfs(self,sr,sc,color,old,newimage,n,m,vis):
+        q = deque()
+        q.append([sr,sc])
+        delc = [0,1,0,-1]
+        delr = [-1,0,1,0]
+        while q:
+            node = q.popleft()
+            newimage[node[0]][node[1]] = color
+            vis[node[0]][node[1]]=1
+            for i in range(4):
+                if node[0]+delr[i]>=0 and node[0]+delr[i]<n and node[1]+delc[i]>=0 and node[1]+delc[i]<m and newimage[node[0]+delr[i]][node[1]+delc[i]]==old and vis[node[0]+delr[i]][node[1]+delc[i]]!=1:
+                    q.append([node[0]+delr[i],node[1]+delc[i]])
+        return newimage
+
+
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        old = image[sr][sc]
+        if old == color:
+            return image
+        newimage = image[:]
+        n = len(newimage)
+        m = len(newimage[0])
+        vis = [[0 for _ in range(m)] for _ in range(n)]
+        return self.bfs(sr,sc,color,old,newimage,n,m,vis)
+        
