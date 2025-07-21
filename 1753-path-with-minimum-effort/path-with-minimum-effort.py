ebc1007e70098514a23c656b8a06dea2820e548e
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-21 17:49:05 +0530

    Time: 327 ms (55.37%) | Memory: 20.4 MB (18.49%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,26 +1,21 @@
 class Solution:
-    def minimumEffortPath(self, heights):
-        if not heights:
-            return 0
-        
-        rows, cols = len(heights), len(heights[0])
-        min_heap = [(0, 0, 0)] # (effort, row, col)
-        max_effort = 0
-        visited = set()
-
-        while min_heap:
-            effort, cur_row, cur_col = heapq.heappop(min_heap)
-
-            max_effort = max(max_effort, effort)
-            if (cur_row, cur_col) == (rows - 1, cols - 1):
-                return max_effort
-            visited.add((cur_row, cur_col))
-
-            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                new_row, new_col = cur_row + dr, cur_col + dc
-
-                if 0 <= new_row < rows and 0 <= new_col < cols and(new_row, new_col) not in visited:
-                    new_effort = abs(heights[new_row][new_col] - heights[cur_row][cur_col])
-                    heapq.heappush(min_heap, (new_effort, new_row, new_col))
-        
-        return max_effort
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows = len(heights)
+        cols = len(heights[0])
+        pq = [(0,0,0)]
+        maxi = 0
+        vis = set()
+        dr = [0,0,1,-1]
+        dc = [1,-1,0,0]
+        while pq:
+            eff, row, col = heapq.heappop(pq)
+            maxi = max(maxi,eff)
+            if (row,col) == (rows-1,cols-1):
+                return maxi
+            vis.add((row,col))
+            for i in range(4):
+                new_row,new_col = row+dr[i],col+dc[i]
+                if new_row>=0 and new_row<rows and new_col>=0 and new_col<cols and (new_row,new_col) not in vis:
+                    new_eff = abs(heights[new_row][new_col]-heights[row][col])
+                    heapq.heappush(pq,(new_eff,new_row,new_col))
+        return 0
