5db174f2e94275e30ce9b67fe63abdb1b38dc435
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-23 11:29:25 +0530

    Time: 24 ms (25.28%) | Memory: 26 MB (6.42%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -1,33 +1,28 @@
+from heapq import heappush, heappop
+
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        graph = [[] for _ in range(n)]
-        for u, v, time in roads:
-            graph[u].append((v, time))
-            graph[v].append((u, time))
-            
-        dist = [float('inf')] * n
+        adj = [[] for _ in range(n)]
+        for u, v, t in roads:
+            adj[u].append([v, t])
+            adj[v].append([u, t])
+
+        q = [[0, 0]]  # (time, node)
+        time = [float('inf')] * n
         ways = [0] * n
-        
-        dist[0] = 0
+        time[0] = 0
         ways[0] = 1
-        
-        pq = [(0, 0)]
-        
-        MOD = 10**9 + 7
-        
-        # Dijkstra's algorithm
-        while pq:
-            d, node = heapq.heappop(pq)
-            
-            if d > dist[node]:
-                continue
-                
-            for neighbor, time in graph[node]:
-                if dist[node] + time < dist[neighbor]:
-                    dist[neighbor] = dist[node] + time
-                    ways[neighbor] = ways[node]
-                    heapq.heappush(pq, (dist[neighbor], neighbor))
-                elif dist[node] + time == dist[neighbor]:
-                    ways[neighbor] = (ways[neighbor] + ways[node]) % MOD
-        
-        return ways[n-1]
+        mod = 10**9 + 7
+
+        while q:
+            t, node = heappop(q)
+
+            for v, t_ in adj[node]:
+                if t + t_ < time[v]:
+                    time[v] = t + t_
+                    heappush(q, [t + t_, v])
+                    ways[v] = ways[node]
+                elif t + t_ == time[v]:
+                    ways[v] = (ways[v] + ways[node]) % mod
+
+        return ways[n - 1]
