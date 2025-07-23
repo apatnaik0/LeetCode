41a7a9d16dfab472364073301a1b6e764921cfc8
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-23 11:31:42 +0530

    Time: 26 ms (18.66%) | Memory: 25.6 MB (19.51%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -1,28 +1,27 @@
-from heapq import heappush, heappop
-
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
         adj = [[] for _ in range(n)]
-        for u, v, t in roads:
-            adj[u].append([v, t])
-            adj[v].append([u, t])
-
-        q = [[0, 0]]  # (time, node)
-        time = [float('inf')] * n
-        ways = [0] * n
+        for u,v,t in roads:
+            adj[u].append([v,t])
+            adj[v].append([u,t])
+        # print(adj)
+        q = [[0,0]]
+        # time = [1e9]*n
+        time = [float('inf')]*n
+        ways = [0]*n
         time[0] = 0
         ways[0] = 1
+        
         mod = 10**9 + 7
-
         while q:
-            t, node = heappop(q)
-
-            for v, t_ in adj[node]:
+            t,node = heappop(q)
+            # if t > time[node]:
+            #     continue
+            for v,t_ in adj[node]:
                 if t + t_ < time[v]:
-                    time[v] = t + t_
-                    heappush(q, [t + t_, v])
+                    time[v] = t+t_
+                    heappush(q,[t+t_,v])
                     ways[v] = ways[node]
                 elif t + t_ == time[v]:
-                    ways[v] = (ways[v] + ways[node]) % mod
-
-        return ways[n - 1]
+                    ways[v] = (ways[v] + ways[node])%mod
+        return ways[n-1]
