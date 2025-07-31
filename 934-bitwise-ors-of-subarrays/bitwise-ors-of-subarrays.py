fbdba5b86455bb32dd461538ec0efa8e22c37fc4
apatnaik0 <80672034+apatnaik0@users.noreply.github.com>
2025-07-31 16:27:31 +0530

    Time: 479 ms (21.43%) | Memory: 45.1 MB (20.24%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
@@ -1,8 +1,10 @@
 class Solution:
-    def subarrayBitwiseORs(self, arr):
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
         res = set()
         cur = set()
         for num in arr:
             cur = {num | x for x in cur} | {num}
             res |= cur
         return len(res)
+
+        
