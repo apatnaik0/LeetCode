86ea1281f9a6677bb03d2a0bb091dae46dca1582
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-08-28 13:33:29 +0530

    Time: 15 ms (37.92%) | Memory: 18 MB (9.17%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -2,17 +2,18 @@ class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
         diag = [[] for _ in range(n*2-1)]
-        for i in range(n-1,-1,-1):
-            for j in range(n-1,-1,-1):
+        for i in range(n):
+            for j in range(n):
                 diag[i-j+n-1].append(grid[i][j])
+        # print(diag)
         for k in range(len(diag)):
             if k < n-1:  
                 diag[k].sort()
             elif k >= n-1:
                 diag[k].sort(reverse=True)
-        for i in range(n-1,-1,-1):
-            for j in range(n-1,-1,-1):
+        for i in range(n):
+            for j in range(n):
                 d = diag[i-j+n-1]
-                grid[i][j] = d.pop()
+                grid[i][j] = d.pop(0)
         return grid
         
