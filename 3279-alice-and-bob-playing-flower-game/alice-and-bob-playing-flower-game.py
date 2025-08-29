7b0f1c46933e8c8000097a0541925bedf44a8e62
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-08-29 12:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.50%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -1,7 +1,3 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        on = (n+1)//2
-        en = (n)//2
-        om = (m+1)//2
-        em = (m)//2
-        return (on*em) + (en*om)
+        return n*m//2
