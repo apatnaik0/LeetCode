3a09767b6151d09609af9e72486f4fce4a51853b
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-09-13 07:43:02 -0700

    Time: 3 ms (65.94%) | Memory: 18 MB (20.04%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        hm = defaultdict(int)
+        for c in s:
+            hm[c]+=1
+        
+        vm,cm = 0,0
+        for k,v in hm.items():
+            if k in 'aeiou':
+                vm = max(vm,v)
+            else:
+                cm = max(cm,v)
+        
+        return vm + cm
