61ddab11298918bfc99fa31a3b71731117592a45
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-09-29 11:06:23 -0700

    Time: 43 ms (48.06%) | Memory: 17.9 MB (69.17%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
@@ -12,9 +12,14 @@ class Solution:
 
     def minScoreTriangulation(self, values: List[int]) -> int:
         n = len(values)
-        dp = [[0 for _ in range(50)] for _ in range(50)]
-        ans = self.helper(dp,0,n-1,values,n)
-        return ans
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        # ans = self.helper(dp,0,n-1,values,n)
+        # return ans
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                for k in range(i + 1, j):
+                    dp[i][j] = min(dp[i][j] if dp[i][j]!=0 else float('inf'), dp[i][k] + dp[k][j] + values[i]*values[j]*values[k])
+        return dp[0][n-1]
 
 
 
