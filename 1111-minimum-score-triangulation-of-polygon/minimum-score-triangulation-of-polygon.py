2eaa5105488bab9775a1b52bf9fbd560148f1329
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-09-29 10:52:52 -0700

    Time: 67 ms (10.83%) | Memory: 18 MB (43.61%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
@@ -1,8 +1,8 @@
 class Solution:
     def helper(self,dp,i,j,values,n):
         ans = 0
-        if j == 0:
-            j = n - 1
+        # if j == 0:
+        #     j = n - 1
         if dp[i][j] != 0:
             return dp[i][j]
         for k in range(i+1,j):
@@ -13,7 +13,7 @@ class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         n = len(values)
         dp = [[0 for _ in range(50)] for _ in range(50)]
-        ans = self.helper(dp,0,0,values,n)
+        ans = self.helper(dp,0,n-1,values,n)
         return ans
 
 
