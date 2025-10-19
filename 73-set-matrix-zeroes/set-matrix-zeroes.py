c937cde3652ade7c46a39e42a509dc03b86c880e
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-10-19 16:37:14 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (37.66%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,42 +1,36 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        rows = len(matrix)
-        cols = len(matrix[0])
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        first_col_zero = 0
+        first_row_zero = 0
 
-        r1z = False
-        c1z = False
+        for i in range(n):
+            if matrix[i][0] == 0:
+                first_col_zero = 1
+                break 
 
-        for i in range(rows):
-            if matrix[i][0]==0:
-                c1z = True
+        for j in range(m):
+            if matrix[0][j] == 0:
+                first_row_zero = 1
                 break
 
-        for i in range(cols):
-            if matrix[0][i]==0:
-                r1z = True
-                break
-        
-        for i in range(1,rows):
-            for j in range(1,cols):
-                if matrix[i][j]==0:
-                    matrix[0][j]=0
-                    matrix[i][0]=0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
 
-        for i in range(1,rows):
-            for j in range(1,cols):
-                if matrix[0][j]==0 or matrix[i][0]==0:
-                    matrix[i][j]=0
-        
-        if c1z:
-            for i in range(rows):
-                matrix[i][0]=0
-        
-        if r1z:
-            for i in range(cols):
-                matrix[0][i]=0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
 
+        if first_row_zero:
+            for j in range(m):
+                matrix[0][j] = 0
 
-        
+        if first_col_zero:
+            for i in range(n):
+                matrix[i][0] = 0
