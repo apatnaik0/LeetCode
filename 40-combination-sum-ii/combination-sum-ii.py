086f564cd967fef4466f7341d688097084623baf
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-09 15:07:02 -0800

    Time: 10 ms (70.31%) | Memory: 17.8 MB (83.65%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,20 +1,22 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def rec(ind,target):
-            if target == 0:
-                ans.append(sub[:])
-                return
-            for i in range(ind,len(candidates)):
-                if i>ind and candidates[i]==candidates[i-1]:
-                    continue
-                if candidates[i]>target:
-                    break
-                sub.append(candidates[i])
-                rec(i+1,target-candidates[i])
+    def rec(self,i,sub,ans,a,t):
+        if t == 0:
+            ans.append(sub[:])
+            return ans
+                
+        for ix in range(i,len(a)):
+            if ix>i and a[ix]==a[ix-1]:
+                continue
+            if a[ix]<=t:
+                sub.append(a[ix])
+                self.rec(ix+1,sub,ans,a,t-a[ix])
                 sub.pop()
+        return ans
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         sub = []
+        i = 0
         candidates.sort()
-        rec(0,target)
+        ans = self.rec(i,sub,ans,candidates, target)
         return ans
-        
