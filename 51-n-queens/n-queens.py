be9a28ea905a62c7961ee3c9caa88d563d17ba4e
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-10 11:52:30 -0800

    Time: 7 ms (96.65%) | Memory: 18.1 MB (96.05%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,28 +1,33 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+    def rec(self, col, board, ans, l ,ld, ud, n):
+        if col == n:
+            ans.append(board[:])
+            return ans
         
-        def rec(col,board,n,left,udiag,ldiag):
-            if col == n:
-                ans.append(board[:])
-                return
-            for row in range(n):
-                if left[row]==0 and ldiag[row+col]==0 and udiag[n-1+col-row]==0:
-                    board[row] = board[row][:col] + 'Q' + board[row][col+1:]
-                    left[row]=1
-                    ldiag[row+col]=1
-                    udiag[n-1+col-row]=1
-                    rec(col+1,board,n,left,udiag,ldiag)
-                    board[row] = board[row][:col] + '.' + board[row][col+1:]
-                    left[row]=0
-                    ldiag[row+col]=0
-                    udiag[n-1+col-row]=0
+        for row in range(n):
+            if l[row] == 0 and ld[row+col]==0 and ud[n-1+col-row]==0:
+                board[row] = board[row][:col] + 'Q' + board[row][col+1:]
+                l[row] = 1
+                ld[row+col] = 1
+                ud[n-1+col-row] = 1
+                self.rec(col+1, board, ans, l, ld, ud, n)
+                board[row] = board[row][:col] + '.' + board[row][col+1:]
+                l[row] = 0
+                ld[row+col] = 0
+                ud[n-1+col-row] = 0
+        return ans
 
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
         ans = []
         board = []
         for i in range(n):
-            board.append('.'*n)
-        left = [0]*n
-        udiag = [0]*(2*n-1)
-        ldiag = [0]*(2*n-1)
-        rec(0,board,n,left,udiag,ldiag)
+            s = '.'*n
+            board.append(s)
+        
+        l = [0]*n
+        ld = [0]*(2*n-1)
+        ud = [0]*(2*n-1)
+
+        ans = self.rec(0,board,ans,l,ld,ud,n)
         return ans
