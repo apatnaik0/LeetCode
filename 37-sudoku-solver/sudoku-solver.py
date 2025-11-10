f3dc39be230bb4902ec82e3cebc0b124d5bf97e8
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-10 13:28:16 -0800

    Time: 1335 ms (62.73%) | Memory: 17.7 MB (94.88%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -1,38 +1,35 @@
 class Solution:
-    def boxind(self,row,col):
-            return (row//3)*3 + col//3
+    def boxind(self,i,j):
+        return (i//3)*3 + j//3
 
-    def solve(self,ind,board,empty, row, col, box):
+    def rec(self, ind, board, row, col, box , empty):
         if ind == len(empty):
             return True
+
         r,c = empty[ind]
-
-        for num in '123456789':
-            if num not in row[r] and num not in col[c] and num not in box[self.boxind(r,c)]:
-                board[r][c] = num
-                row[r].add(num)
-                col[c].add(num)
-                box[self.boxind(r,c)].add(num)
-
-                if self.solve(ind+1,board,empty, row, col, box):
+        
+        for i in '123456789':
+            if i not in row[r] and i not in col[c] and i not in box[self.boxind(r,c)]:
+                board[r][c] = i
+                row[r].add(i)
+                col[c].add(i)
+                box[self.boxind(r,c)].add(i)
+                if self.rec(ind+1, board, row, col, box , empty):
                     return True
-
                 board[r][c] = '.'
-                row[r].remove(num)
-                col[c].remove(num)
-                box[self.boxind(r,c)].remove(num)
-
-        return False
-
+                row[r].discard(i)
+                col[c].discard(i)
+                box[self.boxind(r,c)].discard(i)
+        return
 
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
+        empty = []
         row = [set() for _ in range(9)]
         col = [set() for _ in range(9)]
         box = [set() for _ in range(9)]
-        empty = []
 
         for i in range(9):
             for j in range(9):
@@ -42,7 +39,8 @@ class Solution:
                     row[i].add(board[i][j])
                     col[j].add(board[i][j])
                     box[self.boxind(i,j)].add(board[i][j])
+            
+        self.rec(0, board, row, col, box, empty)
+        
 
-        self.solve(0,board,empty, row, col, box)
-        return board
         
