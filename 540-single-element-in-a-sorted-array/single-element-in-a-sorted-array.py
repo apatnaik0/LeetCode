762b81ad824d30c7ac956d9e89c1e8efb2b3bd63
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-11 09:33:17 -0800

    Time: 0 ms (100.00%) | Memory: 24.9 MB (16.52%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,23 +1,27 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        elif nums[0] != nums[1]:
+            return nums[0]
+        elif nums[n-1]!= nums[n-2]:
+            return nums[n-1]
         
-        if len(nums)==1:
-            return nums[0]
-        elif nums[0]!=nums[1]:
-            return nums[0]
-        elif nums[len(nums)-1]!=nums[len(nums)-2]:
-            return nums[len(nums)-1]
-        low = 1
-        high = len(nums)-2
-        while low<=high:
-            mid = (low+high)//2
-            if nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]:
-                return nums[mid]
-            elif (mid%2==1 and nums[mid-1]==nums[mid]) or (mid%2==0 and nums[mid]==nums[mid+1] ):
-                low = mid+1
+        l = 1
+        r = n-2
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid] != nums[mid-1] and nums[mid]!= nums[mid+1]:
+                    return nums[mid]
+            if mid%2 == 0:
+                if nums[mid] == nums[mid+1]:
+                    l = mid+1
+                else:
+                    r = mid-1
             else:
-                high = mid-1
-        
-
-            
+                if nums[mid] == nums[mid-1]:
+                    l = mid+1
+                else:
+                    r = mid-1
         
