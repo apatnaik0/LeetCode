05412eb500ca938081bcb96ef63953d67078f79d
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-11 10:25:21 -0800

    Time: 4 ms (33.91%) | Memory: 18.2 MB (18.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,31 +3,36 @@ class Solution:
         n1 = len(nums1)
         n2 = len(nums2)
 
-        if n1>n2:
+        if n1 > n2:
             return self.findMedianSortedArrays(nums2,nums1)
-
+        
         low = 0
         high = n1
         left = (n1+n2+1)//2
-        while low<=high:
+
+        while low <= high:
             mid1 = (low+high)//2
             mid2 = left - mid1
-            l1,l2 = float('-inf'),float('-inf')
-            r1,r2 = float('inf'),float('inf')
-            if mid1<n1:
+
+            r1, r2 = float('inf'), float('inf')
+            l1, l2 = float('-inf'), float('-inf')
+
+            if mid1 < n1:
                 r1 = nums1[mid1]
-            if mid2<n2:
+            if mid2 < n2:
                 r2 = nums2[mid2]
-            if mid1-1>=0:
+            if mid1-1 >= 0:
                 l1 = nums1[mid1-1]
-            if mid2-1>=0:
+            if mid2-1 >= 0:
                 l2 = nums2[mid2-1]
+            
             if l1<=r2 and l2<=r1:
-                if (n1+n2)%2==1:
-                    return max(l1,l2)
+                if (n1+n2)%2==0:
+                    return (max(l1,l2) + min(r1,r2))/2
                 else:
-                    return (max(l1,l2)+min(r1,r2))/2
+                    return max(l1,l2)
             elif l1>r2:
                 high = mid1-1
             else:
                 low = mid1+1
+        
