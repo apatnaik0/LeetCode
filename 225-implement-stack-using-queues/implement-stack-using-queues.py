5d2c94e36e875a83cc7b6128b16ea0d58d576c1c
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-13 11:15:14 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (64.35%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,24 +1,22 @@
-from queue import Queue
 class MyStack:
 
     def __init__(self):
-        self.q = Queue()
+        self.q = deque()
 
     def push(self, x: int) -> None:
-        size = self.q.qsize()
-        self.q.put(x)
-        for i in range(size):
-            self.q.put(self.q.get())
+        l = len(self.q)
+        self.q.append(x)
+        for i in range(l):
+            self.q.append(self.q.popleft())
 
     def pop(self) -> int:
-        return self.q.get()
+        return self.q.popleft()
 
     def top(self) -> int:
-        return self.q.queue[0]
+        return self.q[0]
 
     def empty(self) -> bool:
-        return not self.q.qsize()
-        
+        return len(self.q) == 0
 
 
 # Your MyStack object will be instantiated and called as such:
