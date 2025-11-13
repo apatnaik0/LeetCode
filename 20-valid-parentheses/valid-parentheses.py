d11dd79a0e61e3f9dc839aa9a8fbe7f93bb0d732
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-13 12:53:52 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (86.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,17 +2,25 @@ class Solution:
     def isValid(self, s: str) -> bool:
         st = []
         for i in s:
-            if i=='(' or i=='{' or i=='[':
+            if i == '(' or i == '{' or i == '[':
                 st.append(i)
             else:
                 if not st:
                     return False
-                top = st.pop()
-                if (i==')' and top=='(') or (i=='}' and top=='{') or (i==']' and top=='['):
-                    continue
-                else:
-                    return False
-        if not st:
-            return True
-        return False
-        
+                elif i == ')':
+                    if st[-1]=='(':
+                        st.pop()
+                    else:
+                        return False
+                elif i == '}':
+                    if st[-1]=='{':
+                        st.pop()
+                    else:
+                        return False
+                elif i == ']':
+                    if st[-1]=='[':
+                        st.pop()
+                    else:
+                        return False
+        return st == []
+            
