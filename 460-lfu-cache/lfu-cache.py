51a65c0f6b2503184d3e0244748dd5e142fb0e7f
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-15 11:51:21 -0800

    Time: 167 ms (38.87%) | Memory: 79.7 MB (84.56%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,75 +1,80 @@
-class ListNode:
-    def __init__(self, key, val):
+class Node:
+    def __init__(self,key,val):
         self.key = key
         self.val = val
         self.freq = 1
-        self.prev = None
         self.next = None
-        
+        self.prev = None
+
 class DLL:
     def __init__(self):
-        self.head = ListNode(0, 0)
-        self.tail = ListNode(0, 0)
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
         self.size = 0
-        
-    def insertHead(self, node):
-        headNext = self.head.next
-        headNext.prev = node
-        self.head.next = node
+
+    def add(self,node):
+        front = self.head.next
+        node.next = front
         node.prev = self.head
-        node.next = headNext
+        front.prev = node
+        self.head.next = node
         self.size += 1
-        
-    def removeNode(self, node):
-        node.next.prev = node.prev
-        node.prev.next = node.next
-        self.size -= 1
-        
-    def removeTail(self):
-        tail = self.tail.prev
-        self.removeNode(tail)
-        return tail
     
+    def delete(self, node):
+        front = node.next
+        back = node.prev
+        front.prev = back
+        back.next = front
+        self.size -= 1
+    
+    def del_tail(self):
+        tail = self.tail.prev
+        self.delete(tail)
+        return tail
 
 class LFUCache:
 
     def __init__(self, capacity: int):
+        self.min_freq = 0
+        self.cap = capacity
         self.cache = {}
-        self.freqTable = collections.defaultdict(DLL)
-        self.capacity = capacity
-        self.minFreq = 0
-        
-    
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        return self.updateCache(self.cache[key], key, self.cache[key].val)
-        
+        self.ftable = collections.defaultdict(DLL)
 
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            return self.update_cache(key, self.cache[key].val)
+        else:
+            return -1
+            
     def put(self, key: int, value: int) -> None:
-        if not self.capacity:
+        if not self.cap:
             return
         if key in self.cache:
-            self.updateCache(self.cache[key], key, value)
+            self.update_cache(key,value)
         else:
-            if len(self.cache) == self.capacity:
-                prevTail = self.freqTable[self.minFreq].removeTail()
-                del self.cache[prevTail.key]
-            node = ListNode(key, value)
-            self.freqTable[1].insertHead(node)
+            if len(self.cache) == self.cap:
+                prev_tail = self.ftable[self.min_freq].del_tail()
+                del self.cache[prev_tail.key]
+            node = Node(key,value)
+            self.ftable[1].add(node)
             self.cache[key] = node
-            self.minFreq = 1
-        
+            self.min_freq = 1
     
-    def updateCache(self, node, key, value):
+    def update_cache(self, key,value):
         node = self.cache[key]
         node.val = value
-        prevFreq = node.freq
+        prev_freq = node.freq
         node.freq += 1
-        self.freqTable[prevFreq].removeNode(node)
-        self.freqTable[node.freq].insertHead(node)
-        if prevFreq == self.minFreq and self.freqTable[prevFreq].size == 0:
-            self.minFreq += 1
+        self.ftable[prev_freq].delete(node)
+        self.ftable[node.freq].add(node)
+        if prev_freq == self.min_freq and self.ftable[prev_freq].size == 0:
+            self.min_freq += 1
         return node.val
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
