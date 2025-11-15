3025aecbb765d98fef1a5dcfc39d8934699522b8
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-14 18:44:28 -0800

    Time: 3 ms (81.44%) | Memory: 17.6 MB (99.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,38 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        n = len(grid)
-        m = len(grid[0])
-        vis = [[0 for _ in range(m)] for _ in range(n)]
         ct = 0
         fresh = 0
-        t = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 2:
-                    q.append([i,j])
-                    vis[i][j] = 1
-                elif grid[i][j] == 1:
+        m = len(grid)
+        n = len(grid[0])
+        vis = [[ -1 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
                     fresh += 1
-        if fresh==0:
-            return 0
+                elif grid[i][j] == 2:
+                    q.append((i,j))
+                    vis[i][j] = 1
         
+        if fresh == 0:
+            return 0
+        rot = 0
         while q:
-            l = len(q)
-            for i in range(l):
-                node = q.popleft()
-                r = node[0]
-                c = node[1]
-                delr = [-1,0,1,0]
-                delc = [0,1,0,-1]
+            s = len(q)
+            for i in range(s):
+                r,c = q.popleft()
+                dr = [0,0,1,-1]
+                dc = [1,-1,0,0]
                 for i in range(4):
-                    nr = r+delr[i]
-                    nc = c+delc[i]
-                    if nc>=0 and nc<m and nr>=0 and nr<n and grid[nr][nc]==1 and vis[nr][nc]==0:
-                        q.append([nr,nc])
-                        vis[nr][nc]=1
-                        ct+=1
-                # print(q)
-            t+=1
-        print(ct,fresh,t)
-        return t-1 if ct==fresh else -1
+                    nr = r + dr[i]
+                    nc = c + dc[i]
+                    if nr >= 0 and nr < m and nc >= 0 and nc < n and grid[nr][nc]==1 and vis[nr][nc]!= 1:
+                        q.append((nr,nc))
+                        vis[nr][nc] = 1
+                        rot += 1
+            ct += 1
+        
+        return ct-1 if rot == fresh else -1
+
+        
+
