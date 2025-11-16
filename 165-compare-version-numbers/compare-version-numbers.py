30b2edbeb8340323dc494b79de2c8f69d8cc2e58
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-16 13:20:20 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.02%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,14 +1,26 @@
 class Solution:
+    def level(self,v,n):
+        for i in range(n):
+            v.append('0')
+        return v
+
     def compareVersion(self, version1: str, version2: str) -> int:
         v1 = version1.split('.')
         v2 = version2.split('.')
+
         n1 = len(v1)
         n2 = len(v2)
+
+        if n1 > n2:
+            v2 = self.level(v2,n1-n2)
+        elif n2 > n1:
+            v1 = self.level(v1,n2-n1)
+        
         for i in range(max(n1,n2)):
-            r1 = int(v1[i] if i<n1 else 0)
-            r2 = int(v2[i] if i<n2 else 0)
-            if r1>r2:
-                return 1
-            elif r1<r2:
+            if int(v1[i]) < int(v2[i]):
                 return -1
+            elif int(v1[i]) > int(v2[i]):
+                return 1
+        
         return 0
+
