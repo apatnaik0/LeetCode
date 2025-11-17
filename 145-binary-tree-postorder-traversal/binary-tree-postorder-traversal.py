6080f6a370134cb497fefd24f72dceb53103f452
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-17 13:08:25 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.60%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -5,54 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def postorder(self,root, ans):
+        if not root:
+            return ans
         
-        # ans = []
-        # def postorder(node):
-        #     if not node:
-        #         return
-        #     postorder(node.left)
-        #     postorder(node.right)
-        #     ans.append(node.val)
-        # postorder(root)
-        # return ans
-
-        # if root is None:
-        #     return []
-        # st1 = []
-        # st2 = []
-        # st1.append(root)
-        # while st1:
-        #     node = st1.pop()
-        #     st2.append(node)
-        #     if node.left:
-        #         st1.append(node.left)
-        #     if node.right:
-        #         st1.append(node.right)
-        
-        # ans = []
-        # while st2:
-        #     ans.append(st2.pop().val)
-        # return ans
-
-        if root is None:
-            return []
-        curr = root
-        st = []
-        ans = []
-        while st or curr:
-            if curr:
-                st.append(curr)
-                curr = curr.left
-            else:
-                temp = st[-1].right
-                if temp is None:
-                    node = st.pop()
-                    ans.append(node.val)
-                    while st and node == st[-1].right:
-                        node = st.pop()
-                        ans.append(node.val)
-                else:
-                    curr = temp
+        self.postorder(root.left, ans)
+        self.postorder(root.right, ans)
+        ans.append(root.val)
         return ans
-        
+
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        ans = self.postorder(root,ans)
+        return ans
