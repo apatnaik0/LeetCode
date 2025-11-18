59efeae68ee6236d9ee1ac7cd17290b78c736586
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-17 16:39:38 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (61.08%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,35 +6,38 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = {}
+        ans = {}
         q = deque()
+        y = 0
+        mini = 0
+        maxi = 0
         q.append((root,0))
-        mini,maxi = 0,0
-        y=0
         while q:
-            for i in range(len(q)):
-                node,x = q.popleft()
-                if x not in res:
-                    res[x] = {}
-                if y not in res[x]:
-                    res[x][y] = []
-                res[x][y].append(node.val)
+            l = len(q)
+            for i in range(l):
+                node, x = q.popleft()
+                if x not in ans:
+                    ans[x] = {}
+                if y not in ans[x]:
+                    ans[x][y] = []
+                ans[x][y].append(node.val)
                 mini = min(x,mini)
                 maxi = max(x,maxi)
                 if node.left:
-                    q.append((node.left,x-1))
+                    q.append((node.left, x - 1))
                 if node.right:
-                    q.append((node.right,x+1))
-            y+=1
-
-        ans = []
-        for i in range(mini,maxi+1):
-            v_level = []
+                    q.append((node.right, x + 1))
+            y += 1
+        
+        final = []
+        for i in range(mini, maxi + 1):
+            v = []
             for j in range(y):
-                if i in res and j in res[i]:
-                    res[i][j].sort()
-                    v_level.extend(res[i][j])
-            ans.append(v_level)
-        return ans
+                if i in ans and j in ans[i]:
+                    ans[i][j].sort()
+                    v.extend(ans[i][j])
+            final.append(v)
+        
+        return final
 
-
+                
