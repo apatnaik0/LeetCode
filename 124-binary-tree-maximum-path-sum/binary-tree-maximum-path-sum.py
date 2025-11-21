f2ba1a7696b3cd0c22c5d6120ac96a3c1ca25492
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-21 01:41:04 -0800

    Time: 18 ms (45.52%) | Memory: 23 MB (79.24%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -5,14 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def mps(self,root):
+        if not root:
+            return 0
+        
+        ls = max(0,self.mps(root.left))
+        rs = max(0,self.mps(root.right))
+
+        self.maxi = max(self.maxi, root.val + ls + rs)
+        return root.val + max(ls,rs)
+
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        maxi = [root.val]
-        def mps(root):
-            if not root:
-                return 0
-            ls = max(0,mps(root.left))
-            rs = max(0,mps(root.right))
-            maxi[0] = max(maxi[0],ls+rs+root.val)
-            return root.val + max(ls,rs)
-        mps(root)
-        return maxi[0]
+        self.maxi = root.val
+        self.mps(root)
+        return self.maxi
