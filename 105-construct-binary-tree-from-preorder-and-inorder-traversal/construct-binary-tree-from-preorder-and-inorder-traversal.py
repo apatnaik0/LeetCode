90b4a9e142234ee0d2cc7ca35512ace119a5c1f1
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-21 20:57:47 -0800

    Time: 3 ms (87.44%) | Memory: 19.7 MB (55.36%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,22 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    def bt(self,preorder, pres,pree,inorder, ins,ine, indict):
+        if pres > pree or ins > ine:
+            return None
+        rootval = preorder[pres]
+        rootind = indict[rootval]
+        left = rootind-ins
+        root = TreeNode(rootval)
+        root.left = self.bt(preorder,pres+1,pres+left,inorder,ins,rootind-1,indict)
+        root.right = self.bt(preorder,pres+left+1,pree,inorder,rootind+1,ine,indict)
+        return root
+
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         indict = {}
         for i in range(len(inorder)):
-            indict[inorder[i]]=i
+            indict[inorder[i]] = i
         
-        def build(preorder,prestart,preend,inorder,instart,inend,indict):
-            if prestart>preend or instart > inend:
-                return None
-            rootval = preorder[prestart]
-            rootind = indict[rootval]
-            root = TreeNode(rootval)
-            numsleft = rootind - instart
-            
-            root.left =  build(preorder,prestart+1,prestart+numsleft,inorder,instart,rootind-1,indict)
-            root.right =  build(preorder,prestart+numsleft+1,preend,inorder,rootind+1,inend,indict)
-            return root
-
-        root = build(preorder,0,len(preorder)-1,inorder,0,len(inorder),indict)
-        return root
+        return self.bt(preorder,0,len(preorder)-1,inorder,0,len(inorder)-1,indict)
+        
