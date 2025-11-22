fc917c2e8ff5355635678bfae41e2703a10fb8ae
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-21 21:53:30 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (50.80%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -5,21 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def bt(self,inorder, ins,ine, postorder, posts, poste, indict):
+        if posts > poste or ins > ine:
+            return None
+
+        rootval = postorder[poste]
+        rootind = indict[rootval]
+        root = TreeNode(rootval)
+        left = rootind - ins
+
+        root.left = self.bt(inorder, ins, rootind-1, postorder, posts, posts + left-1, indict)
+        root.right = self.bt(inorder, rootind+1, ine, postorder, posts+left, poste-1,indict)
+        return root
+
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         indict = {}
         for i in range(len(inorder)):
             indict[inorder[i]] = i
         
-        def build(inorder,ins,ine,postorder,pos,poe,indict):
-            if pos>poe or ins>ine:
-                return None
-            rootval = postorder[poe]
-            rootind = indict[rootval]
-            numsleft = rootind - ins
-            root = TreeNode(rootval)
-            root.left = build(inorder,ins,rootind-1,postorder,pos,pos+numsleft-1,indict)
-            root.right = build(inorder,rootind+1,ine,postorder,pos+numsleft,poe-1,indict)
-            return root
-        root = build(inorder,0,len(inorder)-1,postorder,0,len(postorder)-1,indict)
-        return root
-        
+        return self.bt(inorder,0, len(inorder)-1, postorder, 0, len(postorder)-1,indict)
