d80b82db6cf1630cda91e062e4a5012aed5d5fb8
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-11-23 18:50:49 -0800

    Time: 6 ms (50.58%) | Memory: 20 MB (29.10%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -5,40 +5,39 @@
 #         self.left = left
 #         self.right = right
 class BSTI:
-    def pushall(self,node):
-        while node:
-            self.st.append(node)
-            if self.rev:
-                node = node.right
-            else:
-                node = node.left
-
     def __init__(self,root,rev):
         self.st = []
         self.rev = rev
         self.pushall(root)
 
+    def pushall(self,root):
+        while root:
+            self.st.append(root)
+            if self.rev:
+                root = root.right
+            else:
+                root = root.left
+
     def next(self):
         node = self.st.pop()
-        temp = node
         if self.rev:
-            self.pushall(temp.left)
+            self.pushall(node.left)
         else:
-            self.pushall(temp.right)
+            self.pushall(node.right)
         return node.val
-        
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        l = BSTI(root,False)
-        r = BSTI(root,True)
-        
-        i = l.next()
-        j = r.next()
-        while i<j:
-            if i+j==k:
+        li = BSTI(root,False)
+        ri = BSTI(root,True)
+
+        lv = li.next()
+        rv = ri.next()
+        while lv < rv:
+            if lv + rv == k:
                 return True
-            elif i+j > k:
-                j = r.next()
+            elif lv + rv > k:
+                rv = ri.next()
             else:
-                i = l.next()
+                lv = li.next()
         return False
