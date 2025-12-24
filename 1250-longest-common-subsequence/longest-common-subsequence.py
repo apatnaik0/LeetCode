72526dad157d6745e6c551f8f895a9dd6b98e34f
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-24 13:19:40 +0530

    Time: 502 ms (47.05%) | Memory: 43.1 MB (41.15%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,31 +1,24 @@
 class Solution:
+    def solve(self,text1,text2,n1,n2,i,j,dp):
+        if i<0 or j<0:
+            return 0
+        if dp[i][j]!=0:
+            return dp[i][j]
+        if text1[i] == text2[j]:
+            ans = 1 + self.solve(text1,text2,n1,n2,i-1,j-1,dp)
+        else:
+            ans = max(self.solve(text1,text2,n1,n2,i-1,j,dp),self.solve(text1,text2,n1,n2,i,j-1,dp))
+        return ans
+
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1 = len(text1)
         n2 = len(text2)
-        def solve(i1,i2):
-            if i1<0 or i2<0:
-                return 0
-            if dp[i1][i2]!=-1:
-                return dp[i1][i2]
-            if text1[i1]==text2[i2]:
-                dp[i1][i2] = 1 + solve(i1-1,i2-1)
-            else:
-                dp[i1][i2] = max(solve(i1,i2-1),solve(i1-1,i2))
-            return dp[i1][i2]
-        # dp = [[-1 for _ in range(n2+1)] for _ in range(n1+1)]
-        # return solve(n1-1,n2-1)
-        prev = [0 for _ in range(n2+1)]
-        # for i in range(n2+1):
-        #     dp[0][i] = 0
-        # for i in range(n1+1):
-        #     dp[i][0] = 0
-        prev[0] = 0
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        # return self.solve(text1,text2,n1,n2,n1-1,n2-1,dp)
         for i in range(1,n1+1):
-            cur = [0 for _ in range(n2+1)]
             for j in range(1,n2+1):
-                if text1[i-1]==text2[j-1]:
-                    cur[j] = 1 + prev[j-1]
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 else:
-                    cur[j] = max(prev[j],cur[j-1])
-            prev = cur
-        return prev[n2]
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[n1][n2]
