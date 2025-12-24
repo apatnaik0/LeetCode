ffe15190191f6109dce6411c3dac0ea31f37db21
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-24 13:22:12 +0530

    Time: 479 ms (51.04%) | Memory: 26 MB (85.28%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -15,10 +15,13 @@ class Solution:
         n2 = len(text2)
         dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
         # return self.solve(text1,text2,n1,n2,n1-1,n2-1,dp)
+        prev = [0 for _ in range(n2+1)]
         for i in range(1,n1+1):
+            curr = [0 for _ in range(n2+1)]
             for j in range(1,n2+1):
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    curr[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-        return dp[n1][n2]
+                    curr[j] = max(prev[j],curr[j-1])
+            prev = curr
+        return curr[n2]
