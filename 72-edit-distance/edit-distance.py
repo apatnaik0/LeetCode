8f67e40b3521d3bc62e4475a1bb32d6de1d93d01
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-24 17:43:51 +0530

    Time: 23 ms (99.07%) | Memory: 19.1 MB (89.88%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,26 +1,21 @@
 class Solution:
+    def solve(self,word1,word2,n1,n2,i,j,dp):
+        if i < 0:
+            return j+1
+        if j < 0:
+            return i+1
+        if dp[i][j] != -1:
+            return dp[i][j]
+        if word1[i] == word2[j]:
+            ans = self.solve(word1,word2,n1,n2,i-1,j-1,dp)
+        else:
+            ans = 1 + min(self.solve(word1,word2,n1,n2,i-1,j-1,dp),self.solve(word1,word2,n1,n2,i-1,j,dp), self.solve(word1,word2,n1,n2,i,j-1,dp))
+        dp[i][j] = ans
+        return dp[i][j]
+
     def minDistance(self, word1: str, word2: str) -> int:
-        
         n1 = len(word1)
         n2 = len(word2)
-        # dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
-        prev = [0 for _ in range(n2+1)]
 
-        # for i in range(n1+1):
-        #     dp[i][0] = i
-        for i in range(n2+1):
-            prev[i] = i
-
-
-
-        for i in range(1,n1+1):
-            cur = [0 for _ in range(n2+1)]
-            cur[0] = i
-            for j in range(1,n2+1):
-                if word1[i-1]==word2[j-1]:
-                    cur[j] = prev[j-1]
-                else:
-                    cur[j] = 1 + min(prev[j-1], cur[j-1], prev[j])
-            prev = cur
-        return prev[n2]
-    
+        dp = [[-1 for _ in range(n2)] for _ in range(n1)]
+        return self.solve(word1,word2,n1,n2,n1-1,n2-1,dp)
