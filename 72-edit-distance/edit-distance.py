9ae32dc6933f16a9c6b97cad3c9484b52e0e3ea5
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-24 18:38:32 +0530

    Time: 79 ms (18.70%) | Memory: 20.8 MB (64.10%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -17,5 +17,16 @@ class Solution:
         n1 = len(word1)
         n2 = len(word2)
 
-        dp = [[-1 for _ in range(n2)] for _ in range(n1)]
-        return self.solve(word1,word2,n1,n2,n1-1,n2-1,dp)
+        dp = [[-1 for _ in range(n2+1)] for _ in range(n1+1)]
+        # return self.solve(word1,word2,n1,n2,n1-1,n2-1,dp)
+        for i in range(n1+1):
+            dp[i][0] = i
+        for j in range(n2+1):
+            dp[0][j] = j
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
+        return dp[n1][n2]
