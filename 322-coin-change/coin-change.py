91b0fb6bc995b96b06ebe65ffdc993dc11b85491
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-25 12:19:01 +0530

    Time: 1387 ms (7.88%) | Memory: 21.4 MB (24.07%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -17,8 +17,21 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         dp = [[0 for _ in range(amount+1)] for _ in range(n)]
-        ans = self.solve(coins,amount,n-1,dp)
-        if ans >= float('inf'):
+        # ans = self.solve(coins,amount,n-1,dp)
+        for i in range(amount+1):
+            if i%coins[0] == 0:
+                dp[0][i] = i//coins[0]
+            else:
+                dp[0][i] = float('inf')
+
+        for i in range(1,n):
+            for j in range(1,amount+1):
+                pick = float('inf')
+                if j >= coins[i]:
+                    pick = 1+dp[i][j-coins[i]]
+                not_pick = dp[i-1][j]
+                dp[i][j] = min(pick,not_pick)
+        if dp[n-1][amount] >= float('inf'):
             return -1
         else:
-            return ans
+            return dp[n-1][amount]
