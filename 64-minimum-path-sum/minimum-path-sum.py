57b16db79459114039f4934be419f4cbcacff082
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-25 10:20:47 +0530

    Time: 35 ms (20.19%) | Memory: 19.7 MB (85.65%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,39 +1,18 @@
 class Solution:
+    def solve(self,grid,i,j,dp):
+        if i == 0 and j == 0:
+            return grid[0][0]
+        if i < 0 or j < 0:
+            return float('inf')
+        if dp[i][j] != -1:
+            return dp[i][j]
+        up = grid[i][j] + self.solve(grid,i-1,j,dp)
+        left = grid[i][j] + self.solve(grid,i,j-1,dp)
+        dp[i][j] = min(up,left)
+        return dp[i][j]
+
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m = len(grid)-1
-        n = len(grid[0])-1
-        # def solve(i,j,dp):
-        #     if i == 0 and j == 0:
-        #         return grid[i][j]
-        #     if i<0 or j<0:
-        #         return int(1e9)
-        #     if dp[i][j]!=-1:
-        #         return dp[i][j]
-        #     left = grid[i][j] + solve(i,j-1,dp)
-        #     up = grid[i][j] + solve(i-1,j,dp)
-        #     dp[i][j] = min(left,up)
-        #     return dp[i][j]
-        # dp = [[-1 for _ in range(n+1)] for _ in range(m+1)]
-        # return solve(m,n,dp)
-        prev = [-1 for _ in range(n+1)]
-        for i in range(m+1):
-            cur = [-1 for _ in range(n+1)]
-            for j in range(n+1):
-                if i==0 and j==0:
-                    cur[j]=grid[i][j]
-                else:
-                    left = grid[i][j]
-                    if j>0:
-                        left += cur[j-1]
-                    else:
-                        left += int(1e9)
-                    up = grid[i][j]
-                    if i>0:
-                        up += prev[j]
-                    else:
-                        up += int(1e9)
-                    cur[j] = min(left,up)
-            prev = cur
-        return prev[n]
-        
-        
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        return self.solve(grid,m-1,n-1,dp)
