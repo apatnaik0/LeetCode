3bffa429baff744af528406ca6ab98e48cb55f17
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-25 12:29:55 +0530

    Time: 1290 ms (9.71%) | Memory: 18.3 MB (54.71%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -16,22 +16,25 @@ class Solution:
 
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        dp = [[0 for _ in range(amount+1)] for _ in range(n)]
+        # dp = [[0 for _ in range(amount+1)] for _ in range(n)]
         # ans = self.solve(coins,amount,n-1,dp)
+        prev = [0 for _ in range(amount+1)]
         for i in range(amount+1):
             if i%coins[0] == 0:
-                dp[0][i] = i//coins[0]
+                prev[i] = i//coins[0]
             else:
-                dp[0][i] = float('inf')
+                prev[i] = float('inf')
 
         for i in range(1,n):
+            curr = [0 for _ in range(amount+1)]
             for j in range(1,amount+1):
                 pick = float('inf')
                 if j >= coins[i]:
-                    pick = 1+dp[i][j-coins[i]]
-                not_pick = dp[i-1][j]
-                dp[i][j] = min(pick,not_pick)
-        if dp[n-1][amount] >= float('inf'):
+                    pick = 1+curr[j-coins[i]]
+                not_pick = prev[j]
+                curr[j] = min(pick,not_pick)
+            prev = curr
+        if prev[amount] >= float('inf'):
             return -1
         else:
-            return dp[n-1][amount]
+            return prev[amount]
