f1ffa7cce2b43aa0b7b98506e203c0b0f4b4f920
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-25 12:10:21 +0530

    Time: 1147 ms (16.98%) | Memory: 19.5 MB (28.98%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,35 +1,24 @@
 class Solution:
+    def solve(self,coins,amt,i,dp):
+        if i == 0:
+            if amt%coins[i] == 0:
+                return amt//coins[i]
+            else:
+                return float('inf')
+        if dp[i][amt]!=0:
+            return dp[i][amt]
+        pick = float('inf')
+        if amt >= coins[i]:
+            pick = 1+self.solve(coins,amt-coins[i],i,dp)
+        not_pick = self.solve(coins,amt,i-1,dp)
+        dp[i][amt] = min(pick,not_pick)
+        return dp[i][amt]
+
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        # def solve(ind,target):
-        #     if ind == 0:
-        #         if target%coins[ind]==0:
-        #             return target//coins[ind]
-        #         return 1e9
-        #     if dp[ind][target]!=-1:
-        #         return dp[ind][target]
-        #     pick = 1e9
-        #     if target>=coins[ind]:
-        #         pick = 1 + solve(ind,target-coins[ind])
-        #     notpick = solve(ind-1,target)
-        #     dp[ind][target] = min(pick,notpick)
-        #     return dp[ind][target]
-        
-        prev = [0 for _ in range(amount+1)]
-        # ans = solve(n-1,amount)
-        for i in range(amount,-1,-1):
-            if i%coins[0]==0:
-                prev[i] = i//coins[0]
-            else:
-                prev[i] = 1e9
-        for i in range(1,n):
-            for j in range(amount+1):
-                pick = 1e9
-                if j>=coins[i]:
-                    pick = 1 + prev[j-coins[i]]
-                notpick = prev[j]
-                prev[j] = min(pick,notpick)
-        if prev[amount]>=1e9:
+        dp = [[0 for _ in range(amount+1)] for _ in range(n)]
+        ans = self.solve(coins,amount,n-1,dp)
+        if ans >= float('inf'):
             return -1
-        return prev[amount]
-            
+        else:
+            return ans
