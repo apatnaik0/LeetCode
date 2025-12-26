29243cce08d2653658a6d4d7450f323401b53335
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-26 20:31:39 +0530

    Time: 821 ms (39.32%) | Memory: 17.8 MB (98.06%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -1,28 +1,18 @@
 class Solution:
+    def solve(self,i,j,cuts,dp):
+        if i > j:
+            return 0
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        mini = float('inf')
+        for ind in range(i,j+1):
+            mini = min(mini, cuts[j+1] - cuts[i-1] + self.solve(i,ind-1,cuts,dp) + self.solve(ind+1,j,cuts,dp))
+        dp[i][j] = mini
+        return dp[i][j]
+
     def minCost(self, n: int, cuts: List[int]) -> int:
-        def solve(i,j):
-            if i>j:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            mini = int(1e9)
-            for ind in range(i,j+1):
-                mini = min(mini,cuts[j+1] - cuts[i-1] + solve(i,ind-1) + solve(ind+1,j))
-            dp[i][j] = mini
-            return dp[i][j]            
-
-
         c = len(cuts)
         cuts.sort()
         cuts = [0] + cuts + [n]
-        dp = [[0 for _ in range(c+2)] for _ in range(c+2)]
-        # return solve(1,c)
-        for i in range(c,0,-1):
-            for j in range(i,c+1):
-                # if i > j:
-                #     continue
-                mini = float('inf')
-                for k in range(i,j+1):
-                    mini = min(mini,cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j])
-                dp[i][j] = mini
-        return dp[1][c]
+        dp = [[-1 for _ in range(c+2)] for _ in range(c+2)]
+        return self.solve(1,c,cuts,dp)
