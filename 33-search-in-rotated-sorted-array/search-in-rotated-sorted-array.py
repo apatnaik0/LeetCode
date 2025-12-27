e5ed696c4fc04b4c48f7e561163d8cc6332c2f86
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-27 13:55:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (94.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,33 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
         low = 0
-        high = n-1
-        
-        while low<=high:
-            mid = (low+high)//2
-            # print(low,mid,high)
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high)//2
             if nums[mid] == target:
                 return mid
-            
-            if nums[mid] >= nums[low]:
-                # print('left sorted')
-                if target >= nums[low] and target < nums[mid]:
-                    # print('in sorted')
-                    high = mid - 1
-                    # print(low,high)
+            if nums[low]<=nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid -1
                 else:
-                    # print('not in sorted')
                     low = mid + 1
-                    # print(low,high)
-            elif nums[high] >= nums[mid]:
-                # print('right sorted')
-                if target > nums[mid] and target <= nums[high]:
-                    # print('in sorted')
+            else:
+                if nums[mid] <= target <= nums[high]:
                     low = mid + 1
-                    # print(low,high)
                 else:
-                    # print('not in sorted')
-                    high = mid - 1
-                    # print(low,high)
+                    high = mid -1
         return -1
