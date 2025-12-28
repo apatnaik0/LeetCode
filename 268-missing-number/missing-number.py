fa8cf37bc83d717d0dd62b1a74842fc6f12218e2
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2025-12-28 13:11:54 +0530

    Time: 3 ms (62.77%) | Memory: 18.6 MB (82.60%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -9,7 +9,8 @@ class Solution:
 
         xor1 = 0
         xor2 = 0
-        for i in range(len(nums)):
+        n = len(nums)
+        for i in range(n):
             xor1 = xor1^nums[i]
             xor2 = xor2^(i+1)
         return xor1^xor2
