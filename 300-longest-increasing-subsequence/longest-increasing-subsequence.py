d61982a91f2f967c189ccbd82d51e190a5ea10ef
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-01-02 12:09:18 +0530

    Time: 7218 ms (5.00%) | Memory: 146.3 MB (5.01%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,25 +1,31 @@
 class Solution:
-    def solve(self,i,pi,nums,n,dp):
+    def solve(self,n,dp,pi,i,nums):
         if i == n:
             return 0
-        if dp[i][pi]!=-1:
-            return dp[i][pi]
-        ans = self.solve(i+1,pi,nums,n,dp)
-        if pi == -1 or nums[i]>nums[pi]:
-            ans = max(ans,1+ self.solve(i+1,i,nums,n,dp))
-        dp[i][pi] = ans
-        return ans
         
+        if dp[pi][i] != 0:
+            return dp[pi][i]
+        
+        pick = float('-inf')
+        if pi == -1 or nums[i]>nums[pi]:
+            pick = 1 + self.solve(n,dp,i,i+1,nums)
+        not_pick = self.solve(n,dp,pi,i+1,nums)
+
+        dp[pi][i] = max(pick,not_pick)
+        return dp[pi][i]
+
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        # dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
-        # return self.solve(0,-1,nums,n,dp)
-        nxt = [0 for _ in range(n+1)]
+        dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+        # return self.solve(n,dp,-1,0,nums)
+        
         for i in range(n-1,-1,-1):
-            curr = [0 for _ in range(n+1)]
             for pi in range(i-1,-2,-1):
-                curr[pi+1] = nxt[pi+1]
+                pick = float('-inf')
                 if pi == -1 or nums[i]>nums[pi]:
-                    curr[pi+1] = max(curr[pi+1],1+ nxt[i+1])
-            nxt = curr
-        return curr[0]
+                    pick = 1 + dp[i+1][i+1]
+                not_pick = dp[i+1][pi+1]
+
+                dp[i][pi+1] = max(pick,not_pick)
+        
+        return dp[0][0]
