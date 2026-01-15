01da3668c232aa8c4f49cea71160b165580b9ad6
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-01-15 13:23:22 -0800

    Time: 3 ms (96.84%) | Memory: 19.4 MB (16.30%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,18 @@
 class Solution:
-    def rec(self,sub, i, ans, candidates, target):
+    def solve(self,i,sub,ans,candidates,target):
         if i == len(candidates):
             if target == 0:
                 ans.append(sub[:])
             return ans
         if candidates[i] <= target:
             sub.append(candidates[i])
-            self.rec(sub, i, ans, candidates, target - candidates[i])
+            self.solve(i,sub,ans,candidates,target-candidates[i])
             sub.pop()
-        self.rec(sub, i+1, ans, candidates, target)
+        self.solve(i+1,sub,ans,candidates,target)
         return ans
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
-        sub = []
-        i = 0
-        ans = self.rec(sub,i, ans, candidates, target)
-        return ans
+        # ans = []
+        # sub = []
+        return self.solve(0,[],[],candidates,target)
+        
