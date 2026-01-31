b0dafc754460885c058b6794d74ca97aba6b5b93
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-01-30 19:58:13 -0800

    Time: 29 ms (67.01%) | Memory: 20.6 MB (42.51%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,25 +1,30 @@
 class Solution:
-    def dfs(self,r,c,vis,m,n,heights):
+    def dfs(self,r,c,vis,heights,m,n):
         if (r,c) in vis:
             return
         vis.add((r,c))
-        for nr, nc in [(r-1,c),(r+1,c),(r,c+1),(r,c-1)]:
+        dr = [1,-1,0,0]
+        dc = [0,0,1,-1]
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
             if 0 <= nr < m and 0 <= nc < n and heights[nr][nc] >= heights[r][c]:
-                self.dfs(nr,nc,vis,m,n,heights)
+                self.dfs(nr,nc,vis,heights,m,n)
 
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
         m = len(heights)
         n = len(heights[0])
+
         pacific = set()
         atlantic = set()
 
         for row in range(m):
-            self.dfs(row,0,pacific,m,n,heights)
-            self.dfs(row,n-1,atlantic,m,n,heights)
+            self.dfs(row,0,pacific,heights,m,n)
+            self.dfs(row,n-1,atlantic,heights,m,n)
 
         for col in range(n):
-            self.dfs(0,col,pacific,m,n,heights)
-            self.dfs(m-1,col,atlantic,m,n,heights)
-
+            self.dfs(0,col,pacific,heights,m,n)
+            self.dfs(m-1,col,atlantic,heights,m,n)
+        
         return list(pacific & atlantic)
