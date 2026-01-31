b9c2a2403ebd30b4029b6f170c423f7badd38ce0
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-01-31 10:32:17 -0800

    Time: 258 ms (35.42%) | Memory: 21.8 MB (40.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,45 +1,33 @@
 class Solution:
-
-    def dfs(self,grid,i,j,m,n):
-        dr = [0,0,1,-1]
-        dc = [1,-1,0,0]
-        for k in range(4):
-            nr = i + dr[k]
-            nc = j + dc[k]
-            if 0 <= nr < m and 0 <= nc < n and self.vis[nr][nc]==0 and grid[nr][nc] == '1':
-                self.vis[nr][nc] = 1
-                self.dfs(grid,nr,nc,m,n)
-
-    def bfs(self,grid,i,j,m,n):
-        dr = [0,0,1,-1]
-        dc = [1,-1,0,0]
+    def bfs(self,grid,i,j,vis,m,n):
         q = deque()
         q.append((i,j))
-        self.vis[i][j] = 1
+        vis[i][j] = 1
+
         while q:
             r,c = q.popleft()
-            for k in range(4):
-                nr = r + dr[k]
-                nc = c + dc[k]
-                if 0 <= nr < m and 0 <= nc < n and self.vis[nr][nc]==0 and grid[nr][nc] == '1':
+            dr = [0,0,1,-1]
+            dc = [1,-1,0,0]
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
+                if 0 <= nr < m and 0 <= nc < n and vis[nr][nc] == 0 and grid[nr][nc] == '1':
                     q.append((nr,nc))
-                    self.vis[nr][nc] = 1
+                    vis[nr][nc] = 1
+
+        return 
 
     def numIslands(self, grid: List[List[str]]) -> int:
         
         m = len(grid)
         n = len(grid[0])
-
-        self.vis = [[0 for _ in range(n)] for _ in range(m)]
+        vis = [[0 for _ in range(n)] for _ in range(m)]
         ct = 0
 
         for i in range(m):
             for j in range(n):
-                # print(grid[i][j])
-                if grid[i][j]=='1' and self.vis[i][j]==0:
-                    # print('inside if')
-                    self.vis[i][j] = 1
-                    self.dfs(grid,i,j,m,n)
+                if grid[i][j] == '1' and vis[i][j] == 0:
+                    self.bfs(grid,i,j,vis,m,n)
                     ct += 1
-        
+
         return ct
