c4ca850b7bb3e0c2c7eb7953b4ec667ca999e642
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-21 18:34:30 -0700

    Time: 19 ms (15.44%) | Memory: 20.4 MB (13.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!= len(t):
-            return False
-
-        tmap = defaultdict(int)
-        smap = defaultdict(int)
-
-        for i,j in zip(s,t):
-            smap[i] += 1
-            tmap[j] += 1
+        return sorted(s) == sorted(t)
         
-        if len(tmap)!=len(smap):
-            return False
-        
-        for i in tmap.keys():
-            if tmap[i] != smap[i]:
-                return False
-        
-        return True
