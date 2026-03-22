44f2ba9ca5c2636583f602bdf69fc2f4aac92243
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-21 19:19:33 -0700

    Time: 55 ms (91.57%) | Memory: 19.7 MB (62.99%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,26 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        thash = {}
-        for i in t:
-            thash[i] = thash.get(i,0) + 1
-        
+        tmap = defaultdict(int)
+        for char in t:
+            tmap[char] += 1
+        ct = len(t)
+
+        n = len(s)
         l = 0
-        r = 0
-        ind = -1
-        mini = float('inf')
-        ct = 0
-        while r<len(s):
-            if thash.get(s[r],0)>0:
-                ct += 1
-            thash[s[r]] = thash.get(s[r],0) - 1
-            while ct==len(t):
-                if r-l+1<mini:
-                    mini = r-l+1
-                    ind = l
-                thash[s[l]] = thash.get(s[l],0) + 1
-                if thash[s[l]]>0:
-                    ct-=1
-                l+=1
-            r+=1
-        return "" if ind == -1 else s[ind:ind+mini]
+        ans = float('inf')
+        
+        for r in range(n):
+            if s[r] in tmap:
+                if tmap[s[r]] > 0:
+                    ct -= 1
+                tmap[s[r]] -= 1
             
+            while ct == 0:
+
+                if r - l + 1 < ans:
+                    ans = r-l+1
+                    start = l
+                
+                if s[l] in tmap:
+                    tmap[s[l]] += 1
+                    if tmap[s[l]] > 0:
+                        ct += 1
+                
+                l += 1
+
+        return "" if ans == float('inf') else s[start:start+ans]
