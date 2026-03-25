524dabe4876a5069a37a16fa09522f12718fc403
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-24 23:49:34 -0700

    Time: 6 ms (33.06%) | Memory: 20.6 MB (53.10%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,26 +1,26 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = [0]*numCourses
+        ans = []
         adj = [[] for _ in range(numCourses)]
-        topo = []
-        indegree = [0] * numCourses
-        for i in prerequisites:
-            course = i[0]
-            prereq = i[1]
-            adj[prereq].append(course)
-            indegree[course]+=1
+
+        n = len(prerequisites)
+        for i,j in prerequisites:
+            indegree[i] += 1
+            adj[j].append(i)
 
         q = deque()
-        for i in range(len(indegree)):
-            if indegree[i]==0:
+        for i in range(numCourses):
+            if indegree[i] == 0:
                 q.append(i)
 
+        
         while q:
-            node = q.popleft()
-            topo.append(node)
-            for i in adj[node]:
-                indegree[i] -= 1
-                if indegree[i]==0:
-                    q.append(i)
-        
-        return topo if len(topo)==numCourses else []
-        
+            cur = q.popleft()
+            ans.append(cur)
+            for neighbour in adj[cur]:
+                indegree[neighbour] -= 1
+                if indegree[neighbour] == 0:
+                    q.append(neighbour)
+
+        return ans if len(ans) == numCourses else []
