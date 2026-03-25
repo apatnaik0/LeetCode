dddb8b4fd29214f84381ae58ba35f4ceec935b4a
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-25 15:24:30 -0700

    Time: 6 ms (34.34%) | Memory: 19 MB (99.93%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,17 +1,25 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = defaultdict(set)
-        col = defaultdict(set)
-        box = defaultdict(set)
+        row_count = [set() for _ in range(9)]
+        col_count = [set() for _ in range(9)]
+        box_count = [set() for _ in range(9)]
 
         for i in range(9):
             for j in range(9):
-                ele = board[i][j]
-                if ele == '.':
-                    continue
-                if ele in row[i] or ele in col[j] or ele in box[(i//3,j//3)]:
-                    return False
-                row[i].add(ele)
-                col[j].add(ele)
-                box[(i//3,j//3)].add(ele)
+                if board[i][j].isnumeric():
+                    if board[i][j] in row_count[i]:
+                        return False
+                    else:
+                        row_count[i].add(board[i][j])
+
+                    if board[i][j] in col_count[j]:
+                        return False
+                    else:
+                        col_count[j].add(board[i][j])
+                    
+                    if board[i][j] in box_count[(i//3)*3 + j//3]:
+                        return False
+                    else:
+                        box_count[(i//3)*3 + j//3].add(board[i][j])
+        
         return True
