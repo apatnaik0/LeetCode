99f31d4aaa1427bd920e575792541dd1da957da7
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-24 22:40:36 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (75.88%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,30 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        q = deque()
-        newimage = image[:]
-        old = image[sr][sc]
-        if old == color:
+        if image[sr][sc] == color:
             return image
         n = len(image)
         m = len(image[0])
-        vis = [[0 for _ in range(m)] for _ in range(n)]
-        newimage[sr][sc] = color
+        q = deque()
         q.append((sr,sc))
-        delr = [0,0,1,-1]
-        delc = [1,-1,0,0]
+        old_color = image[sr][sc]
+        image[sr][sc] = color
+        dr = [0,0,1,-1]
+        dc = [1,-1,0,0]
 
         while q:
-            l = len(q)
-            for _ in range(l):
-                r,c = q.popleft()
-                
-                for d in range(4):
-                    nr = r + delr[d]
-                    nc = c + delc[d]
-
-                    if 0 <= nr < n and 0<= nc < m and vis[nr][nc]==0 and newimage[nr][nc] == old:
-                        newimage[nr][nc] = color
-                        q.append((nr,nc))
-                        vis[nr][nc] = 1
-        return newimage
-                
+            r,c = q.popleft()
+            
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
+                if 0 <= nr < n and 0 <= nc < m and image[nr][nc] == old_color:
+                    image[nr][nc] = color
+                    q.append((nr,nc))
+        
+        return image
