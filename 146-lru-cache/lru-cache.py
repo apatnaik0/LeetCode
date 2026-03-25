f845d3b76801cd0b1a0493d2ebb5e324297f2554
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-25 09:19:49 -0700

    Time: 118 ms (64.67%) | Memory: 78.9 MB (10.93%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self,key, val):
+    def __init__(self,key = 0, val = 0):
         self.key = key
         self.val = val
         self.next = None
@@ -8,25 +8,27 @@ class Node:
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
         self.head = Node(0,0)
         self.tail = Node(0,0)
+        self.cap = capacity
+        self.cache = {}
         self.head.next = self.tail
         self.tail.prev = self.head
-    
-    def add(self, node):
-        front = self.head.next
-        node.next = front
-        node.prev = self.head
-        front.prev = node
-        self.head.next = node
 
-    def delete(self, node):
+    def add(self,node):
+        front = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = front
+        front.prev = node
+        return
+
+    def delete(self,node):
         front = node.next
         back = node.prev
         back.next = front
         front.prev = back
+        return
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -40,12 +42,17 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.delete(self.cache[key])
-        self.cache[key] = Node(key,value)
-        self.add(self.cache[key])
+        new_node = Node(key,value)
+        self.cache[key] = new_node
+
         if len(self.cache) > self.cap:
             lru = self.tail.prev
             self.delete(lru)
             del self.cache[lru.key]
+        
+        self.add(self.cache[key])
+        return
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
