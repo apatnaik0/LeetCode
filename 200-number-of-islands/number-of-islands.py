024d8cfafe39704c181be5ac1f04ca03e9ae61d0
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-25 11:18:26 -0700

    Time: 259 ms (31.11%) | Memory: 22.1 MB (33.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,27 @@
 class Solution:
-    def bfs(self,grid,i,j,vis,m,n):
-        q = deque()
-        q.append((i,j))
-        vis[i][j] = 1
-
-        while q:
-            r,c = q.popleft()
-            dr = [0,0,1,-1]
-            dc = [1,-1,0,0]
-            for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-                if 0 <= nr < m and 0 <= nc < n and vis[nr][nc] == 0 and grid[nr][nc] == '1':
-                    q.append((nr,nc))
-                    vis[nr][nc] = 1
-
-        return 
-
     def numIslands(self, grid: List[List[str]]) -> int:
         
         m = len(grid)
         n = len(grid[0])
         vis = [[0 for _ in range(n)] for _ in range(m)]
-        ct = 0
+        islands = 0
+        dr = [0,0,1,-1]
+        dc = [1,-1,0,0]
+
+        def dfs(r,c):
+            for k in range(4):
+                nr = r + dr[k]
+                nc = c + dc[k]
+                if 0<=nr<m and 0<=nc<n and vis[nr][nc] == 0 and grid[nr][nc] == '1':
+                    vis[nr][nc] = 1
+                    dfs(nr,nc)
+
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and vis[i][j] == 0:
-                    self.bfs(grid,i,j,vis,m,n)
-                    ct += 1
-
-        return ct
+                if grid[i][j] == '1' and vis[i][j]==0:
+                    vis[i][j] = 1
+                    dfs(i,j)
+                    islands += 1
+        
+        return islands
