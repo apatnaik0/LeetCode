c52bf2b81bd2c06cd2799f9a7427b0fd2c53140a
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-25 12:03:11 -0700

    Time: 116 ms (60.92%) | Memory: 22.1 MB (67.46%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,29 +1,30 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n = len(mat)
-        m = len(mat[0])
-        vis = [[0 for _ in range(m)] for _ in range(n)]
-        q = deque()
-        for i in range(n):
-            for j in range(m):
-                if mat[i][j]==0:
-                    q.append([i,j,0])
-                    vis[i][j]=1
-        ans = [[0 for _ in range(m)] for _ in range(n)]
-        while q:
-            node = q.popleft()
-            r = node[0]
-            c = node[1]
-            d = node[2]
-            ans[r][c]=d
-            delr = [0,0,1,-1]
-            delc = [-1,1,0,0]
-            for i in range(4):
-                nr = r+delr[i]
-                nc = c+delc[i]
-                if nr>=0 and nr<n and nc>=0 and nc<m and vis[nr][nc]==0:
-                    q.append([nr,nc,d+1])
-                    vis[nr][nc]=1
-            # print(ans)
-        return ans
+        m = len(mat)
+        n = len(mat[0])
 
+        q = deque()
+        dist = [[-1 for _ in range(n)] for _ in range(m)]
+
+        dr = [0, 0, 1, -1]
+        dc = [1, -1, 0, 0]
+
+        # Start BFS from all 0s
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                    dist[i][j] = 0
+
+        while q:
+            r, c = q.popleft()
+
+            for k in range(4):
+                nr = r + dr[k]
+                nc = c + dc[k]
+
+                if 0 <= nr < m and 0 <= nc < n and dist[nr][nc] == -1:
+                    dist[nr][nc] = dist[r][c] + 1
+                    q.append((nr, nc))
+
+        return dist
