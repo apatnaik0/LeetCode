337e811c2271319455731627aec2ddbf96c814b2
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-24 23:15:06 -0700

    Time: 4 ms (35.89%) | Memory: 19.3 MB (88.91%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,38 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        ct = 0
         fresh = 0
-        m = len(grid)
-        n = len(grid[0])
-        vis = [[ -1 for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
+        rotten = deque()
+        rot = 0
+        dr = [0,0,1,-1]
+        dc = [1,-1,0,0]
+        n = len(grid)
+        m = len(grid[0])
+        vis = [[0 for _ in range(m)] for _ in range(n)]
+        minutes = 0
+
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == 1:
                     fresh += 1
                 elif grid[i][j] == 2:
-                    q.append((i,j))
+                    rotten.append((i,j))
+                    # rot += 1
                     vis[i][j] = 1
-        
-        if fresh == 0:
-            return 0
-        rot = 0
-        while q:
-            s = len(q)
-            for i in range(s):
-                r,c = q.popleft()
-                dr = [0,0,1,-1]
-                dc = [1,-1,0,0]
+
+        while rotten and fresh > 0:
+            l = len(rotten)
+            for _ in range(l):
+                r,c = rotten.popleft()
                 for i in range(4):
                     nr = r + dr[i]
                     nc = c + dc[i]
-                    if nr >= 0 and nr < m and nc >= 0 and nc < n and grid[nr][nc]==1 and vis[nr][nc]!= 1:
-                        q.append((nr,nc))
+                    if 0 <= nr < n and 0 <= nc < m and vis[nr][nc] == 0 and grid[nr][nc] == 1:
                         vis[nr][nc] = 1
-                        rot += 1
-            ct += 1
-        
-        return ct-1 if rot == fresh else -1
+                        rotten.append((nr,nc))
+                        fresh -= 1
+            minutes += 1
 
+
+        return minutes if fresh == 0 else -1
         
 
