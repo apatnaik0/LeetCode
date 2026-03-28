7f3c440d5f798edc59ae2e76c2f77c65c5b3a721
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-28 08:02:49 -0700

    Time: 121 ms (39.31%) | Memory: 57 MB (56.51%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -2,24 +2,33 @@ import random
 class RandomizedSet:
 
     def __init__(self):
+        self.arr = []
         self.hmap = {}
         
-
     def insert(self, val: int) -> bool:
         if val in self.hmap:
             return False
         
-        self.hmap[val] = 0
+        self.arr.append(val)
+        self.hmap[val] = len(self.arr) - 1
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.hmap:
             return False
+
+        idx = self.hmap[val]
+        last_val = self.arr[-1]
+
+        self.arr[idx] = last_val
+        self.hmap[last_val] = idx
+
+        self.arr.pop()
         del self.hmap[val]
         return True
 
     def getRandom(self) -> int:
-        return random.choice(list(self.hmap.keys()))        
+        return random.choice(self.arr)        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
