5fc4109f81d97d6e10bd4ee937992bee87014875
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-29 09:48:36 -0700

    Time: 3 ms (31.52%) | Memory: 22.2 MB (73.79%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,24 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    def inorder(self,root):
+        if not root:
+            return
+
+        
+        self.inorder(root.left)
+        self.ct -= 1
+        if self.ct == 0:
+            self.ans = root.val
+            return
+        self.inorder(root.right)
+        return
+
+
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        cur = root
-        while cur:
-            if cur.left:
-                node = cur.left
-                while node.right and node.right!=cur:
-                    node = node.right
-                if not node.right:
-                    node.right = cur
-                    cur = cur.left
-                if node.right == cur:
-                    node.right = None
-                    k -= 1
-                    if k == 0:
-                        return cur.val
-                    cur = cur.right
-            else:
-                k -= 1
-                if k == 0:
-                    return cur.val
-                cur = cur.right
+
+        self.ans = 0
+        self.ct = k
+        self.inorder(root)
+        return self.ans
+
+        
