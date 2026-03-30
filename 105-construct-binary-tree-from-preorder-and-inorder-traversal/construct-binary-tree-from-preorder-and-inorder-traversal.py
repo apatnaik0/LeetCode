0283919f317e9547538295cee3ab5bc291e84c1e
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-30 08:43:02 -0700

    Time: 0 ms (100.00%) | Memory: 21 MB (73.09%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,22 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-    def bt(self,preorder, pres,pree,inorder, ins,ine, indict):
-        if pres > pree or ins > ine:
+    def build(self,preorder,inorder,preorder_start,preorder_end,inorder_start,inorder_end,indict):
+        if preorder_start>preorder_end or inorder_start>inorder_end:
             return None
-        rootval = preorder[pres]
+
+        rootval = preorder[preorder_start]
         rootind = indict[rootval]
-        left = rootind-ins
-        root = TreeNode(rootval)
-        root.left = self.bt(preorder,pres+1,pres+left,inorder,ins,rootind-1,indict)
-        root.right = self.bt(preorder,pres+left+1,pree,inorder,rootind+1,ine,indict)
-        return root
+        node = TreeNode(rootval)
+        nums_left = rootind - inorder_start
+
+        node.left = self.build(preorder,inorder,preorder_start+1,preorder_start+nums_left,inorder_start,rootind - 1,indict)
+        node.right = self.build(preorder,inorder,preorder_start+nums_left+1,preorder_end,rootind + 1,inorder_end,indict)
+
+        return node
 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        preorder_start = 0
+        preorder_end = len(preorder) - 1
+        inorder_start = 0
+        inorder_end = len(inorder) - 1
+
         indict = {}
-        for i in range(len(inorder)):
+        for i in range(inorder_end+1):
             indict[inorder[i]] = i
-        
-        return self.bt(preorder,0,len(preorder)-1,inorder,0,len(inorder)-1,indict)
+
+        return self.build(preorder,inorder,preorder_start,preorder_end,inorder_start,inorder_end,indict)
+
+
+
         
