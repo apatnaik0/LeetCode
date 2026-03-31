f048e53b6f623b79ba0b59366d623c1f5aae9b8b
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-31 14:57:03 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (80.92%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,30 +1,20 @@
 class Solution:
-    def solve(self,arr):
-        prev = arr[0]
+    def helper(self,nums):
+        prev = nums[0]
         prev2 = 0
 
-        for i in range(1,len(arr)):
-            pick = arr[i]
-            if i >1 :
-                pick += prev2
+        for i in range(1,len(nums)):
+            pick = nums[i] + prev2
             notpick = prev
             cur = max(pick,notpick)
             prev2 = prev
             prev = cur
-        
-        return prev
+
+        return prev 
 
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        arr1 = []
-        arr2 = []
         if n == 1:
             return nums[0]
+        return max(self.helper(nums[:n-1]),self.helper(nums[1:n]))
         
-        for i in range(n):
-            if i != 0:
-                arr1.append(nums[i])
-            if i != n-1:
-                arr2.append(nums[i])
-        
-        return max(self.solve(arr1), self.solve(arr2))
