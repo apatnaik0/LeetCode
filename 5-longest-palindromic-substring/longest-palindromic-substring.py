d589fc0873f7bba9fd9ab2a2400df6dccdadd46d
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-03-31 19:51:55 -0700

    Time: 231 ms (81.52%) | Memory: 19.2 MB (95.43%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,22 +1,26 @@
 class Solution:
-    def efc(self,l,r,s):
-        while l>= 0 and r<len(s) and s[l]==s[r]:
-            l -=1
-            r+=1
+    def expand(self,s,l,r):
+        while l>=0 and r <len(s) and s[l]==s[r]:
+            l -= 1
+            r += 1
+
         return s[l+1:r]
-
+            
     def longestPalindrome(self, s: str) -> str:
-        if len(s)<=1:
+        n = len(s)
+
+        if n <= 1:
             return s
-        
-        maxi = ''
 
-        for i in range(len(s)-1):
-            odd = self.efc(i,i,s)
-            even = self.efc(i,i+1,s)
+        ans = ""
 
-            if len(even) > len(maxi):
-                maxi = even
-            if len(odd) > len(maxi):
-                maxi = odd
-        return maxi
+        for i in range(n):
+            odd = self.expand(s,i,i)
+            even = self.expand(s,i,i+1)
+
+            if len(odd)>len(ans):
+                ans = odd
+            if len(even) > len(ans):
+                ans = even
+
+        return ans
