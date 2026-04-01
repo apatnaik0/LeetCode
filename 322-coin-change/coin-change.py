66327af1b97bc2c6b7cd92686db34910e2a04efd
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-04-01 09:54:29 -0700

    Time: 1178 ms (8.41%) | Memory: 23.6 MB (23.08%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -26,9 +26,26 @@ class Solution:
         n = len(coins)
         dp = [[0 for _ in range(amount+1)] for _ in range(n)]
 
-        ans = self.helper(dp,n,coins,amount,n-1)
+        # ans = self.helper(dp,n,coins,amount,n-1)
 
-        if ans >= float('inf'):
+        for i in range(amount+1):
+            if i%coins[0] == 0:
+                dp[0][i] = i//coins[0]
+            else:
+                dp[0][i] = float('inf')
+
+        for i in range(1,n):
+            for j in range(amount+1):
+                pick = float('inf')
+                if coins[i]<=j:
+                    pick = 1 + dp[i][j-coins[i]]
+                notpick = dp[i-1][j]
+                dp[i][j] = min(pick,notpick)
+        if dp[n-1][amount] >= float('inf'):
             return -1
-        else:
-            return ans
+        return dp[n-1][amount]
+
+        # if ans >= float('inf'):
+        #     return -1
+        # else:
+        #     return ans
