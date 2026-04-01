032931be49c626419b8405464c8bc4204bdd5291
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-04-01 09:55:53 -0700

    Time: 1080 ms (10.35%) | Memory: 19.9 MB (36.49%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -24,26 +24,29 @@ class Solution:
 
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        dp = [[0 for _ in range(amount+1)] for _ in range(n)]
+        # dp = [[0 for _ in range(amount+1)] for _ in range(n)]
+        prev = [0 for _ in range(amount+1)]
 
         # ans = self.helper(dp,n,coins,amount,n-1)
 
         for i in range(amount+1):
             if i%coins[0] == 0:
-                dp[0][i] = i//coins[0]
+                prev[i] = i//coins[0]
             else:
-                dp[0][i] = float('inf')
+                prev[i] = float('inf')
 
         for i in range(1,n):
+            cur = [0 for _ in range(amount+1)]
             for j in range(amount+1):
                 pick = float('inf')
                 if coins[i]<=j:
-                    pick = 1 + dp[i][j-coins[i]]
-                notpick = dp[i-1][j]
-                dp[i][j] = min(pick,notpick)
-        if dp[n-1][amount] >= float('inf'):
+                    pick = 1 + cur[j-coins[i]]
+                notpick = prev[j]
+                cur[j] = min(pick,notpick)
+            prev = cur
+        if prev[amount] >= float('inf'):
             return -1
-        return dp[n-1][amount]
+        return prev[amount]
 
         # if ans >= float('inf'):
         #     return -1
