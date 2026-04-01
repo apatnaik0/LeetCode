493d99913c0929796096e357f96edeaa5a0e6b2c
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-04-01 09:42:11 -0700

    Time: 996 ms (13.50%) | Memory: 21.7 MB (28.30%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,45 +1,34 @@
 class Solution:
-    def solve(self,dp,coins,i,amt):
+
+    def helper(self,dp,n,coins,amount,i):
+
         if i == 0:
-            if amt % coins[i] == 0:
-                return amt // coins[i]
+            if amount % coins[i] == 0:
+                return amount//coins[i]
             else:
                 return float('inf')
-        
-        if dp[i][amt]!=0:
-            return dp[i][amt]
-        
+
+        if dp[i][amount]!=0:
+            return dp[i][amount]
+
         pick = float('inf')
-        if amt >= coins[i]:
-            pick = 1+self.solve(dp,coins,i,amt-coins[i])
-        not_pick = self.solve(dp,coins,i-1,amt)
-        
-        dp[i][amt] = min(pick,not_pick)
-        return dp[i][amt]
+        if coins[i] <= amount:
+            pick = 1 + self.helper(dp,n,coins,amount-coins[i],i)
+        notpick = self.helper(dp,n,coins,amount,i-1)
+
+        dp[i][amount] = min(pick,notpick)
+
+        return dp[i][amount]
+
+
 
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        # dp = [[0 for _ in range(amount+1)] for _ in range(n)]
-        # ans = self.solve(dp,coins,n-1,amount)
-        prev = [0 for _ in range(amount+1)]
-        for amt in range(amount+1):
-            if amt % coins[0] == 0:
-                prev[amt] = amt // coins[0]
-            else:
-                prev[amt] = float('inf')
-        
-        for i in range(1,n):
-            cur = [0 for _ in range(amount+1)]
-            for amt in range(amount+1):
-                pick = float('inf')
-                if amt >= coins[i]:
-                    pick = 1+cur[amt-coins[i]]
-                not_pick = prev[amt]
-                
-                cur[amt] = min(pick,not_pick)
-            prev = cur
+        dp = [[0 for _ in range(amount+1)] for _ in range(n)]
 
-        if prev[amt] >= float('inf'):
+        ans = self.helper(dp,n,coins,amount,n-1)
+
+        if ans >= float('inf'):
             return -1
         else:
-            return prev[amt]
+            return ans
