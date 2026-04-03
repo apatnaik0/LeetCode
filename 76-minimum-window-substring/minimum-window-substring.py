8e7ad5f86388466508127bf95d666280676dfc96
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-04-02 23:49:10 -0700

    Time: 55 ms (91.57%) | Memory: 19.6 MB (83.85%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,31 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         tmap = defaultdict(int)
+        ct = 0
         for char in t:
             tmap[char] += 1
-        ct = len(t)
+            ct += 1
 
-        n = len(s)
-        l = 0
         ans = float('inf')
-        
+        l = 0
+        n = len(s)
+
         for r in range(n):
             if s[r] in tmap:
                 if tmap[s[r]] > 0:
                     ct -= 1
                 tmap[s[r]] -= 1
-            
-            while ct == 0:
 
-                if r - l + 1 < ans:
-                    ans = r-l+1
+            while ct == 0:
+                if r-l+1 < ans:
+                    ans = r - l + 1
                     start = l
-                
+
                 if s[l] in tmap:
                     tmap[s[l]] += 1
-                    if tmap[s[l]] > 0:
+                    if tmap[s[l]]>0:
                         ct += 1
-                
+
                 l += 1
 
-        return "" if ans == float('inf') else s[start:start+ans]
+        return '' if ans == float('inf') else s[start:start+ans]
