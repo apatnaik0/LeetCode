bb4c28c84ca7156ca81f7627b86766df3956b40e
Anish Patnaik <80672034+apatnaik0@users.noreply.github.com>
2026-04-02 20:29:51 -0700

    Time: 0 ms (100.00%) | Memory: 19.9 MB (89.49%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,7 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         n = len(nums)
-
+        ans = 0
         low = 0
         high = n-1
 
@@ -10,8 +10,10 @@ class Solution:
             if nums[mid] == target:
                 return mid
             elif nums[mid]<target:
+                ans = mid + 1
                 low = mid + 1
             else:
+                ans = mid
                 high = mid-1
 
-        return low
+        return ans
